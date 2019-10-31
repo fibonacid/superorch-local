3922f0736df76b99e87c99d3a2a705f6fe2ffb64
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-10-31 16:00:22 +0100

    ADDED: connected status

--- src/reducers/baseReducer.js
+++ src/reducers/baseReducer.js
@@ -1,5 +1,20 @@
+const defaultState = {
+    connected: false,
+    activeChat: undefined
+};
+
 export const baseReducer = (state={}, action) => {
     switch(action.type) {
+        case "REDUX_WEBSOCKET::OPEN":
+            return {
+                ...state,
+                connected: action.payload.isTrusted
+            };
+        case "REDUX_WEBSOCKET::CLOSED":
+            return {
+                ...state,
+                connected: false
+            };
         default:
             return state;
     }
