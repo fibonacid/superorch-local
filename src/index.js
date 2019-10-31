ec01277a8aa237afd9bf603b4baedc256304dd6d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-10-31 16:14:36 +0100

    ADDED: configuration of dotenv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Load environment variables
+require('dotenv').config();
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
