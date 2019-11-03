f3c3e9be50592b207621401e8b742d33bcbca5b7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-03 22:32:22 +0100

    FIXED: wrong action name

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -2,7 +2,7 @@ import {actionTypes} from "../actions/actionTypes";
 
 export const usersReducer = (state=[], action) => {
   switch(action.type) {
-    case actionTypes.USERS_LIST:
+    case actionTypes.USER_LIST:
       return action.users;
     default:
       return state;
