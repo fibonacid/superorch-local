b83bf894751931b7e4b4e11e5932a84b6d522be9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-03 22:30:46 +0100

    CHANGED: clean code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Provider} from "react-redux";
 import configureStore from "./store";
-import { connect } from '@giantmachines/redux-websocket';
 import styled, { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset';
 import normalize from "styled-normalize";
