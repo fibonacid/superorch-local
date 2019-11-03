8d7cf7600d9840cc92e61a4306c3b1bf3708dd59
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-03 18:29:12 +0100

    REMOVED: old code

--- src/reducers/baseReducer.js
+++ src/reducers/baseReducer.js
@@ -1,20 +1,9 @@
 const defaultState = {
     connected: false,
-    activeChat: 1
 };
 
 export const baseReducer = (state=defaultState, action) => {
     switch(action.type) {
-        case "REDUX_WEBSOCKET::OPEN":
-            return {
-                ...state,
-                connected: action.payload.isTrusted
-            };
-        case "REDUX_WEBSOCKET::CLOSED":
-            return {
-                ...state,
-                connected: false
-            };
         default:
             return state;
     }
