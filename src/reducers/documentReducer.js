8364ef3c92fd7010907b1d1fa1629072cb586b74
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-03 17:49:41 +0100

    ADDED: reducer method for MESSAGE_RECEIVED action

--- src/reducers/documentReducer.js
+++ src/reducers/documentReducer.js
@@ -1,3 +1,5 @@
+import {actionTypes} from "../actions/actionTypes";
+
 const defaultState = {
   current: "",
   modified: false,
@@ -5,6 +7,11 @@ const defaultState = {
 
 export const documentReducer = (state=defaultState, action) => {
   switch(action.type) {
+    case actionTypes.MESSAGE_RECEIVED:
+        return {
+          ...state,
+          current: state.current + action.message
+        };
     default:
       return state;
   }
