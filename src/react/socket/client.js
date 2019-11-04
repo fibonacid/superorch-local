34c9dcced41aab9994e26f21ae4c08fed9889989
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-04 22:00:03 +0100

    ADDED: fallback websocket url

--- src/react/socket/client.js
+++ src/react/socket/client.js
@@ -5,7 +5,9 @@ require('ws');
 
 export const setupSocket = (dispatch, username) => {
   // Create Socket Client
-  const socket = new WebSocket(process.env['REACT_APP_SOCKET_URL']);
+  const socket = new WebSocket(
+    process.env['REACT_APP_SOCKET_URL'] || 'ws://localhost:8989'
+  );
 
   // ----------------------
   // DEFINE EVENT LISTENERS
