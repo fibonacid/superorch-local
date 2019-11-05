6869d750cc3677a27cf3d59dbe3a160ad775f3d9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-06 00:23:34 +0100

    CHANGED: clean code

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StyledContainer, StyledTitle} from './styles/Header.styles';
 
-const Header = props => (
-  <StyledContainer>
-    <StyledTitle>ColliderChat</StyledTitle>
-  </StyledContainer>
-);
+function Header(props) {
+  return (
+    <StyledContainer>
+      <StyledTitle>ColliderChat</StyledTitle>
+    </StyledContainer>
+  )
+}
 
 export default Header;
