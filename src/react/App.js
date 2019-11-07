6b97e0fde45716ac354df5c0e5954435a2833614
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-07 01:16:40 +0100

    CHANGED: use flashInfo instead of addNotification

--- src/react/App.js
+++ src/react/App.js
@@ -3,8 +3,7 @@ import {Provider} from "react-redux";
 import configureStore from "./store";
 import { channels } from '../shared/constants';
 import * as styled from './App.styles';
-import {addNotification, appInfo} from "./actions/actions";
-import {notificationTypes} from "./utils/constants";
+import {appInfo, flashInfo} from "./actions/actions";
 
 // Containers
 import Document from "./containers/Document";
@@ -30,14 +29,14 @@ const { ipcRenderer } = window;
 ipcRenderer.on('update_available', () => {
   ipcRenderer.removeAllListeners('update_available');
   let message = 'A new update is available. Downloading now...';
-  store.dispatch(addNotification(message, notificationTypes.INFO))
+  store.dispatch(flashInfo(message))
 });
 
 
 ipcRenderer.on('update_downloaded', () => {
   ipcRenderer.removeAllListeners('update_downloaded');
   let message = 'Update Downloaded. It will be installed on restart. Restart now?';
-  store.dispatch(addNotification(message, notificationTypes.INFO))
+  store.dispatch(flashInfo(message))
 });
 
 
