36254c541643d18e4f7426c203bf554f9081f785
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-07 17:01:05 +0100

    ADDED: tests

--- src/react/components/Document.test.js
+++ src/react/components/Document.test.js
@@ -4,58 +4,69 @@ import Document from './Document';
 import {findByTestAttr} from "../utils/testing";
 import toJson from 'enzyme-to-json';
 
-const setUp = (props={}) => {
-  const component = shallow(<Document {...props} />);
-  return component;
+const requiredProps = {
+  shared: "/* Initial Text */"
 };
+
+function setUp(props=requiredProps) {
+  return shallow(
+    <Document {...props}/>
+  )
+}
 
 describe('Document Component', () => {
 
-  describe('has props', () => {
-    let wrapper;
-    beforeEach(() => {
-      const props = {
-        document: {
-          shared: "foo"
-        },
-        send: () => {}
-      };
-      wrapper = setUp(props);
-    });
+  it('Should render without errors', () => {
+    const wrapper = setUp();
+    const component = findByTestAttr(wrapper, 'DocumentComponent');
+    expect(component.length).toBe(1);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 
-    it('Should render without errors', () => {
-      const component = findByTestAttr(wrapper, 'DocumentComponent');
-      expect(component.length).toBe(1);
-      expect(toJson(wrapper)).toMatchSnapshot();
-    });
+  it('Should render a textarea', () => {
+    const wrapper = setUp();
+    const textarea = findByTestAttr(wrapper, 'textarea');
+    expect(textarea.length).toBe(1);
+  });
 
-    it('Should render a textarea', () => {
-      const textarea = findByTestAttr(wrapper, 'textarea');
-      expect(textarea.length).toBe(1);
-    });
+  it('Should render a button', () => {
+    const wrapper = setUp();
+    const button = findByTestAttr(wrapper, 'button');
+    expect(button.length).toBe(1);
+  });
 
-    it('Should render a button', () => {
-      const button = findByTestAttr(wrapper, 'button');
-      expect(button.length).toBe(1);
-    });
+  describe('When new props arrive', () => {
 
+    it('Should merge shared text with local text', () => {
+      const wrapper = setUp();
+      wrapper.setState({ local: "foo" });
+      wrapper.setProps({ shared: "bar" });
+      wrapper.instance().componentWillReceiveProps();
+      //expect(wrapper.state('local')).toMatch(/bar/);
+      expect(wrapper.state('local')).toMatch(/foo(bar)/);
+    });
+  });
+
+  describe('When user enters some text', () => {
+    const fakeEvent = { target: { value: "hello" } };
     it('Should store text input into the state', () => {
+      const wrapper = setUp();
       const textarea = findByTestAttr(wrapper, 'textarea');
-      textarea.simulate('change', {
-        target: { value: "hello" }
-      });
+      textarea.simulate('change', fakeEvent);
       expect(wrapper.state('local')).toEqual('hello');
     });
   });
 
-  describe('has NO props', () => {
-    let wrapper;
-    beforeEach(() => {
-      wrapper = setUp();
-    });
-    it('Should render without errors', () => {
-      const component = findByTestAttr(wrapper, 'DocumentComponent');
-      expect(component.length).toBe(1);
-    }).todo();
-  });
+  describe('When user clicks the button', () => {
+    it.skip('should fire the send prop function', () => {
+      const wrapper = shallow(<Document />);
+      const send = jest.fn();
+      console.log(wrapper.debug())
+      wrapper.setProps({send});
+      const button = findByTestAttr(wrapper, 'button');
+      button.simulate('click');
+      expect(send).toHaveBeenCalledTimes(1);
+    })
+  })
+
 });
