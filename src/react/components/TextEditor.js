b31c6ecc0290b3897f95b00c4ff1edee62787eb9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-08 11:49:49 +0100

    ADDED: wrapper and default Rich Styles

--- src/react/components/TextEditor.js
+++ src/react/components/TextEditor.js
@@ -1,6 +1,10 @@
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js';
 import React from "react";
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
+
+const StyledWrapper = styled.div`
+  flex: 1;
+`;
 
 class TextEditor extends React.Component {
   constructor(props) {
@@ -8,18 +12,37 @@ class TextEditor extends React.Component {
     this.state = {
       editorState: EditorState.createEmpty()
     };
+    this.onChange = this.onChange.bind(this);
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
 
   onChange(editorState) {
     this.setState({editorState});
+    const content = editorState.getCurrentContent();
+    console.log(convertToRaw(content));
   }
 
   render() {
-    return <Editor
-      editorState={this.state.editorState}
-      onChange={this.onChange.bind(this)}
-    />;
+    return (
+      <StyledWrapper>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleKeyCommand={this.handleKeyCommand}
+        />
+      </StyledWrapper>
+    )
   }
 }
+
 
 export default TextEditor;
