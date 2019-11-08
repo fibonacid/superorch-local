da0f89ce9c60c03306e1513f9d0fbc479a665594
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-08 17:36:23 +0100

    ADDED: sagas to send text inputs and code block executions

---
+++ src/react/__tests__/sagas/textInputSaga.test.js
@@ -0,0 +1,8 @@
+import { expectSaga } from 'redux-saga-test-plan';
+import * as matchers from 'redux-saga-test-plan/matchers';
+import { throwError } from 'redux-saga-test-plan/providers';
+import {textInputSaga} from "../../saga/textInputSaga";
+
+describe('textInput (Saga)', () => {
+  it('sends the message', () => {}).todo();
+});
