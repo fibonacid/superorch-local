2526c74362a6ae78992c5ec9b1a0c94b053edb3b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-25 22:55:59 +0100

    ADDED container for socket settings UI

---
+++ src/react/containers/SocketSettings.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {connect as connectSocket} from '@giantmachines/redux-websocket';
+import InputURL from "../components/InputURL";
+
+function SocketSettings(props) {
+  return (
+    <div>
+      <InputURL onClick={props.connect}/>
+    </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  connect: (value) => dispatch(connectSocket(value))
+});
+
+const mapStateToProps = (state) => ({
+  success: state.websocket.connected,
+  initialValue: state.websocket.url
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SocketSettings)
