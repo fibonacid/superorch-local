3e8775317a8485f580538cc354ff64ee1bf10001
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-25 22:54:55 +0100

    CHANGED: styles

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import styled from 'styled-components/macro'
 import PropTypes from 'prop-types';
+import SocketSettings from "../containers/SocketSettings";
 
 const StyledContainer = styled.div`
-  font-size: 20px;
+  font-size: 18px;
   font-weight: bold;
   padding: 5px;
   flex: 0 0 auto;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
 `;
 
 const StyledTitle = styled.h1`
-  text-align: center; 
+  /*text-align: center;*/ 
 `;
 
 function Header(props) {
