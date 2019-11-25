98dd25dd853151ee8907cb8f872d3de034b902b4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-25 22:58:12 +0100

    REMOVED: direct socket call

--- src/react/saga/textInputSaga.js
+++ src/react/saga/textInputSaga.js
@@ -1,11 +1,10 @@
 import {actionTypes} from "../actions/actionTypes";
-import {takeEvery} from "redux-saga/effects";
+import {takeEvery, select} from "redux-saga/effects";
 
-export function* textInputWatcher(params) {
-  yield takeEvery(actionTypes.TEXT_INPUT, textInputSaga, params);
+export function* textInputWatcher() {
+  yield takeEvery(actionTypes.TEXT_INPUT, textInputSaga);
 }
 
-export function* textInputSaga(params, action) {
-  yield action.author = params.username;
-  params.socket.send(JSON.stringify(action))
+export function* textInputSaga(action) {
+  const { username } = yield select(state => state.websocket);
 }
