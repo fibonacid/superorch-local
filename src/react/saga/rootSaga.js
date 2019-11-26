c775a8d6c4b061acc9dadb3d562a775369ba920b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-26 12:59:19 +0100

    FIXED: sagas must be executed inside the effect

--- src/react/saga/rootSaga.js
+++ src/react/saga/rootSaga.js
@@ -1,9 +1,8 @@
-import {all} from "redux-saga/effects";
-import {sendUsernameWatcher} from "./sendUsername";
+import {all, call} from "redux-saga/effects";
+import {sendUsernameSaga, sendUsernameWatcher} from "./sendUsername";
 
 export function* rootSaga() {
-  console.log('rootSaga');
   yield all([
-    sendUsernameWatcher
+    sendUsernameWatcher()
   ])
 }
