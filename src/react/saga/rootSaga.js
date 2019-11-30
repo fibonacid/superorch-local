6e7dd0111940317e199c947736d630507fdb06eb
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-30 21:48:51 +0100

    ADDED: watcher saga for PUSH_DOCUMENT

--- src/react/saga/rootSaga.js
+++ src/react/saga/rootSaga.js
@@ -1,10 +1,12 @@
 import {all} from "redux-saga/effects";
 import {sendUsernameWatcher} from "./sendUsername";
 import {receiveMessageWatcher} from "./receiveMessage";
+import {pushDocumentWatcher} from "./pushDocument";
 
 export function* rootSaga() {
   yield all([
     sendUsernameWatcher(),
-    receiveMessageWatcher()
+    receiveMessageWatcher(),
+    pushDocumentWatcher()
   ])
 }
