9dda81a7d399bb3912c440e01a6763a4e440aff7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-30 16:00:37 +0100

    REMOVED: tests for deprecated sagas

--- src/react/__tests__/sagas/addMessageSaga.test.js
+++
@@ -1,8 +0,0 @@
-import { expectSaga } from 'redux-saga-test-plan';
-import * as matchers from 'redux-saga-test-plan/matchers';
-import { throwError } from 'redux-saga-test-plan/providers';
-import {addMessageSaga} from "../../saga/addMessageSaga";
-
-describe('addMessageSaga', () => {
-  it('sends the message', () => {}).todo();
-});
