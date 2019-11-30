719c4612ac7eeeb58c06de5b2130eba59149f336
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-11-30 16:25:27 +0100

    ADDED: when there is a prop type error, tests should fail

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,9 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import 'jest-prop-type-error'
 
 // Note: this filename must remain 'setupTest'
 // because it's the default path for jest setup
 // in create-react-app
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
