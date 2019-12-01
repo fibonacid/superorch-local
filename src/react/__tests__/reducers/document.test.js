de4cf99fd89c5345b94fdb5cce49c6c03f7ea5c0
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-01 16:36:17 +0100

    CHANGED: removed old tests

--- src/react/__tests__/reducers/document.test.js
+++ src/react/__tests__/reducers/document.test.js
@@ -1,6 +1,4 @@
 import document, {initialState} from '../../reducers/chat/document';
-import {actionTypes} from "../../actions/actionTypes";
-import _ from 'lodash'
 
 describe('document (Reducer)', () => {
 
@@ -8,53 +6,4 @@ describe('document (Reducer)', () => {
     const result = document(undefined, {type:"foo"});
     expect(result).toEqual(initialState);
   });
-
-  it('Should handle MESSAGE_RECEIVED', () => {
-    const action = {
-      type: actionTypes.MESSAGE_RECEIVED,
-      data: { message: 'foo' }
-    };
-    const expectedResult = _.merge({}, initialState, { shared: "foo" });
-    const result = document(undefined, action);
-    expect(result).toEqual(expectedResult);
-  });
-
-
-  it('Should handle TEXT_INPUT_RECEIVED', () => {
-    const action = {
-      type: actionTypes.TEXT_INPUT_RECEIVED,
-      data: { value: 'foo', author: "pablo" }
-    };
-    const expectedResult = _.merge({}, initialState, {
-      input: { value: 'foo', author: "pablo" }
-    });
-    const result = document(undefined, action);
-    expect(result).toEqual(expectedResult);
-  });
-
-
-  it('Should handle EXEC_TEXT_RECEIVED', () => {
-    const action = {
-      type: actionTypes.EXEC_TEXT_RECEIVED,
-      data: { value: 'foo', author: "pablo" }
-    };
-    const expectedResult = _.merge({}, initialState, {
-      exec: { value: 'foo', author: "pablo" }
-    });
-    const result = document(undefined, action);
-    expect(result).toEqual(expectedResult);
-  });
-
-  describe('when state is already populated', () => {
-    it('Should handle MESSAGE_RECEIVED', () => {
-
-    }).todo();
-    it('Should handle TEXT_INPUT_RECEIVED', () => {
-
-    }).todo();
-    it('Should handle EXEC_TEXT_RECEIVED', () => {
-
-    }).todo();
-  });
-
 });
