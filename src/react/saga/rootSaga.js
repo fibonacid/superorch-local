c93f7898e01972d1b5898d8803dbb9b6acc4cda9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-01 17:49:13 +0100

    ADDED: saga to send document state

--- src/react/saga/rootSaga.js
+++ src/react/saga/rootSaga.js
@@ -1,10 +1,12 @@
 import {all} from "redux-saga/effects";
 import {sendUsernameWatcher} from "./sendUsername";
 import {receiveMessageWatcher} from "./receiveMessage";
+import {sendDocumentWatcher} from "./sendDocument";
 
 export function* rootSaga() {
   yield all([
     sendUsernameWatcher(),
-    receiveMessageWatcher(),
+    sendDocumentWatcher(),
+    receiveMessageWatcher()
   ])
 }
---
+++ src/react/saga/sendDocument.js
@@ -0,0 +1,14 @@
+import {put, takeEvery} from "redux-saga/effects";
+import {actionTypes} from "../actions/actionTypes";
+import {send} from '@giantmachines/redux-websocket';
+
+export function* sendDocumentWatcher() {
+  yield takeEvery(actionTypes.SEND_DOCUMENT, sendDocumentSaga);
+}
+
+export function* sendDocumentSaga(action) {
+  // Send it as a message
+  yield put(send({
+    type: "PUSH_DOCUMENT"
+  }))
+}
