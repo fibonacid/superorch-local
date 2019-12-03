7cc9d7896a439fb3473a259cb3804ac2dd16b03b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-03 14:49:47 +0100

    ADDED: send query inside an object with type EXECUTE_SCLANG_QUERY

--- src/react/saga/sendSCLangQuery.js
+++ src/react/saga/sendSCLangQuery.js
@@ -1,8 +1,16 @@
 import { actionTypes } from "../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { put, takeLatest } from "redux-saga/effects";
+import { send } from "@giantmachines/redux-websocket/dist";
 
 export function* sendSCLangQueryWatcher() {
   yield takeLatest(actionTypes.SEND_SCLANG_QUERY, sendSCLangQuerySaga);
 }
 
-export function* sendSCLangQuerySaga(action) {}
+export function* sendSCLangQuerySaga(action) {
+  yield put(
+    send({
+      type: "EXECUTE_SCLANG_QUERY",
+      data: action.query
+    })
+  );
+}
