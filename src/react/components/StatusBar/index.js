64e1b9c2d1f72d48e7cb41a7721db252fda62176
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-03 18:00:37 +0100

    CHANGED: update renamed state property

--- src/react/components/StatusBar/index.js
+++ src/react/components/StatusBar/index.js
@@ -1,10 +1,10 @@
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import StatusBar from "./StatusBar";
 
 const mapStateToProps = state => ({
   appName: state.app.name,
   appVersion: state.app.version,
-  isConnected: state.websocket.connected
+  isConnected: state.websocket.isConnected
 });
 
 export default connect(mapStateToProps, null)(StatusBar);
