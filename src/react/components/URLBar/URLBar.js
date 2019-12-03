55a112e709d4894e71c5c0ce76a8a197461909e7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-03 19:03:14 +0100

    ADDED: icons and logic

--- src/react/components/URLBar/URLBar.js
+++ src/react/components/URLBar/URLBar.js
@@ -1,43 +1,91 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes, css } from "styled-components";
 import { resetInput } from "../../utils/styles";
 import { IconContext } from "react-icons";
 import { IoMdRefresh, IoMdCheckmark, IoMdClose } from "react-icons/io";
 
 const StyledContainer = styled.div`
   border: solid 1px black;
+  border-radius: 5px;
 `;
 
 const StyledInput = styled.input`
   ${resetInput}
   border: none;
   width: 200px;
+  text-indent: 10px;
+  font-size: 18px;
+  line-height: 1.2;
 `;
 
-export function URLBar(props) {
-  const [value, setValue] = useState(props.url);
+const rotate = keyframes`
+  0%   { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
+`;
 
-  function handleClick() {
-    //props.onClick(value);
+const StyledWrapper = styled.div`
+  margin-right: 5px;
+  display: inline-block;
+`;
+
+const StyledLoading = styled(IoMdRefresh)`
+  cursor: pointer;
+  animation: 0.5s ${rotate} linear infinite;
+`;
+
+const StyledRefresh = styled(IoMdRefresh)`
+  cursor: pointer;
+`;
+
+const StyledSuccess = styled(IoMdCheckmark)`
+  cursor: normal;
+`;
+
+const StyledError = styled(IoMdClose)`
+  cursor: pointer;
+`;
+
+/**
+ *
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+function URLBar(props) {
+  const [originalUrl] = useState(props.url);
+  const [url, setUrl] = useState(props.url);
+  const [modified, setModified] = useState(false);
+
+  useEffect(() => {
+    setModified(url !== props.url);
+  }, [url, props.url]);
+
+  function handleReconnect() {
+    props.connect(url);
   }
 
   return (
-    <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
-      <StyledContainer data-test={"InputUrlComponent"}>
-        <StyledInput
-          data-test={"input"}
-          type="text"
-          onChange={e => setValue(e.target.value)}
-          value={value}
-        />
-
-        {props.isLoading && <IoMdRefresh onClick={handleClick} />}
-        {props.isConnected && !props.isLoading && (
-          <IoMdCheckmark color={"green"} />
-        )}
-        {!props.isConnected && !props.isLoading && <IoMdClose color={"red"} />}
-      </StyledContainer>
-    </IconContext.Provider>
+    <StyledContainer data-test={"InputUrlComponent"}>
+      <StyledInput
+        data-test={"input"}
+        type="text"
+        placeholder={originalUrl}
+        onChange={e => setUrl(e.target.value)}
+        value={url}
+      />
+      <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
+        <StyledWrapper>
+          {modified && <StyledRefresh onClick={handleReconnect} />}
+          {!modified && props.isLoading && <StyledLoading />}
+          {!modified && props.isConnected && !props.isLoading && (
+            <StyledSuccess color={"green"} />
+          )}
+          {!modified && !props.isConnected && !props.isLoading && (
+            <StyledError color={"red"} onClick={handleReconnect} />
+          )}
+        </StyledWrapper>
+      </IconContext.Provider>
+    </StyledContainer>
   );
 }
 
