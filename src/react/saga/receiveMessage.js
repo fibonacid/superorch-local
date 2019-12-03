b10ed9e75046d94919d7b9aeb96f3f6cedece57c
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-03 15:07:50 +0100

    ADDED: handler for messages of type EXECUTE_SCLANG_QUERY

--- src/react/saga/receiveMessage.js
+++ src/react/saga/receiveMessage.js
@@ -1,7 +1,8 @@
-import {actionTypes} from "../actions/actionTypes";
-import {takeEvery, put} from "redux-saga/effects";
-import {populateUserList} from "../actions/usersActions";
-import {updateDocument} from "../actions/updateDocument";
+import { actionTypes } from "../actions/actionTypes";
+import { takeEvery, put } from "redux-saga/effects";
+import { populateUserList } from "../actions/usersActions";
+import { updateDocument } from "../actions/updateDocument";
+import { executeSCLangQuery } from "../actions/executeSclangQuery";
 
 /**
  * Every time a connection is opened send your username to the server
@@ -14,12 +15,15 @@ export function* receiveMessageSaga(action) {
   const { message } = action.payload;
   let data = JSON.parse(message);
 
-  switch(data.type) {
-    case 'USER_LIST_UPDATE':
+  switch (data.type) {
+    case "USER_LIST_UPDATE":
       yield put(populateUserList(data.users));
       break;
-    case 'DOCUMENT_UPDATE':
+    case "DOCUMENT_UPDATE":
       yield put(updateDocument(data.data));
+      break;
+    case "EXECUTE_SCLANG_QUERY":
+      yield put(executeSCLangQuery(data.data));
       break;
     default:
       break;
