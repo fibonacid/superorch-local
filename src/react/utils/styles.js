caaec8d020cf1e7962773de7d6eb5df1ff93c0a5
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-03 17:52:32 +0100

    ADDED: helper css to reset input style

--- src/react/utils/styles.js
+++ src/react/utils/styles.js
@@ -1,11 +1,23 @@
-export function resetAppearance() {
-  return (`
+import { css } from "styled-components";
+
+export const resetInput = css`
+  border: solid 1px black;
+  background-image: none;
+  background-color: transparent;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  outline: 0px !important;
+  -webkit-appearance: none;
   &:focus,
   &:hover,
   &:active {
-    outline:0px !important;
-    -webkit-appearance:none;
-    box-shadow: none !important;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    outline: 0px !important;
+    -webkit-appearance: none;
   }
-  `)
-}
+`;
