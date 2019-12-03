f47bb0167077430b2e8fe2efe1a36cdf8198aeca
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-03 12:45:34 +0100

    ADDED: map dispatch of SEND_SCLANG_QUERY to prop handleExecuteSelection

--- src/react/components/TextEditor/index.js
+++ src/react/components/TextEditor/index.js
@@ -1,13 +1,17 @@
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 import TextEditor from "./TextEditor";
-import {sendDocument} from "../../actions/sendDocument";
+import { sendDocument } from "../../actions/sendDocument";
+import { sendSCLangQuery } from "../../actions/sendSCLangQuery";
 
 const mapStateToProps = state => ({
   document: state.chat.document
 });
 
 const mapDispatchToProps = dispatch => ({
-  textInput: (data) => dispatch(sendDocument(data)),
+  handleExecuteSelection: text => {
+    dispatch(sendSCLangQuery(text));
+  },
+  sendEditorState: data => dispatch(sendDocument(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TextEditor);
