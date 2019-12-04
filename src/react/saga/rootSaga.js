f53cc5c79483bbcc119b27587f06c2c723c84056
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-04 16:04:31 +0100

    REMOVED: deprecated saga sendUserWatcher

--- src/react/saga/rootSaga.js
+++ src/react/saga/rootSaga.js
@@ -3,13 +3,11 @@ import { receiveMessageWatcher } from "./receiveMessage";
 import { sendDocumentWatcher } from "./sendDocument";
 import { sendSCLangQueryWatcher } from "./sendSCLangQuery";
 import { executeSCLangQueryWatcher } from "./executeSCLangQuery";
-import { sendUserWatcher } from "./sendUser";
 import { sendUserUpdateWatcher } from "./sendUserUpdate";
 
 export function* rootSaga() {
   yield all([
     executeSCLangQueryWatcher(),
-    sendUserWatcher(),
     sendUserUpdateWatcher(),
     sendDocumentWatcher(),
     sendSCLangQueryWatcher(),
