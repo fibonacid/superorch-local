1dc6f8fa9bb65938b4743454603349e408998f3d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-04 14:34:16 +0100

    ADDED: watcher sagas to sendUser and sendUserUpdate

--- src/react/saga/rootSaga.js
+++ src/react/saga/rootSaga.js
@@ -1,14 +1,16 @@
 import { all } from "redux-saga/effects";
-import { sendUsernameWatcher } from "./sendUsername";
 import { receiveMessageWatcher } from "./receiveMessage";
 import { sendDocumentWatcher } from "./sendDocument";
 import { sendSCLangQueryWatcher } from "./sendSCLangQuery";
 import { executeSCLangQueryWatcher } from "./executeSCLangQuery";
+import { sendUserWatcher } from "./sendUser";
+import { sendUserUpdateWatcher } from "./sendUserUpdate";
 
 export function* rootSaga() {
   yield all([
     executeSCLangQueryWatcher(),
-    sendUsernameWatcher(),
+    sendUserWatcher(),
+    sendUserUpdateWatcher(),
     sendDocumentWatcher(),
     sendSCLangQueryWatcher(),
     receiveMessageWatcher()
