37066677d585e79c7d90ea07b65c7bfcee755bb6
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-04 14:08:24 +0100

    CHANGED: addUser action helper

--- src/react/__tests__/actions/usersActions.test.js
+++ src/react/__tests__/actions/usersActions.test.js
@@ -1,9 +1,14 @@
-import {actionTypes} from "../../actions/actionTypes";
-import {addUser} from "../../actions/usersActions";
+import { actionTypes } from "../../actions/actionTypes";
+import { addUser } from "../../actions/usersActions";
 
-describe('addUser (Action)', () => {
-  it('works', () => {
-    const expectedResult = { type: actionTypes.ADD_USER, name: 'foo' };
-    expect(addUser('foo')).toEqual(expectedResult);
+describe("addUser (Action)", () => {
+  it("works", () => {
+    const result = addUser(1, { name: "foo" });
+    const expectedResult = {
+      type: actionTypes.ADD_USER,
+      id: 1,
+      data: { name: "foo" }
+    };
+    expect(result).toEqual(expectedResult);
   });
 });
