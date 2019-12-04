781970d53527999f8f90e7dfe5ce1dd39da8be44
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-04 18:24:00 +0100

    CHANGED: clean code

--- src/react/saga/userAccepted.js
+++ src/react/saga/userAccepted.js
@@ -1,16 +1,15 @@
 import { put, select, takeLatest } from "redux-saga/effects";
 import { actionTypes } from "../actions/actionTypes";
 import { selectUserByLocalId, selectUsers } from "../reducers/root";
+import { send } from "@giantmachines/redux-websocket";
 
 export function* userAcceptedWatcher(action) {
   yield takeLatest(actionTypes.USER_ACCEPTED, userAcceptedSaga);
 }
 
 export function* userAcceptedSaga() {
-  // Get username.
-  const users = yield select(state => selectUsers(state));
+  // Get main user.
   const user = yield select(state => selectUserByLocalId(state, 0));
-
   // Send it as a message
   yield put(
     send({
