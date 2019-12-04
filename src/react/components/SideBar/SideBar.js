5ab4dcea641a5c485b75f07acb88c4c70077d5ec
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-04 15:58:16 +0100

    ADDED: display user id for debugging

--- src/react/components/SideBar/SideBar.js
+++ src/react/components/SideBar/SideBar.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import styled from 'styled-components/macro'
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components/macro";
 
 const StyledContainer = styled.div`
   border-right: solid 1px black;
@@ -18,21 +18,19 @@ const StyledListItem = styled.li`
   border-bottom: solid 1px black;
 `;
 
-
 function SideBar(props) {
-    const {users} = props;
-    return (
-      <StyledContainer
-        data-test={'SideBarComponent'} >
-        <StyledList data-test={'list'} >
-          {users.map((user,i) => (
-            <StyledListItem data-test={'list-item'} key={i}>
-                {user.name}
-            </StyledListItem>
-          ))}
-        </StyledList>
-      </StyledContainer>
-    )
+  const { users } = props;
+  return (
+    <StyledContainer data-test={"SideBarComponent"}>
+      <StyledList data-test={"list"}>
+        {users.map((user, i) => (
+          <StyledListItem data-test={"list-item"} key={i}>
+            {user.name} #{user.id}
+          </StyledListItem>
+        ))}
+      </StyledList>
+    </StyledContainer>
+  );
 }
 
 SideBar.propTypes = {
