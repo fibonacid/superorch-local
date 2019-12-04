c6e4347b1e9aa5363bf6ebf1593cfacec71c492f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-04 12:01:45 +0100

    CHANGED: removed 'Should have a title' test

--- src/react/components/Header/Header.test.js
+++ src/react/components/Header/Header.test.js
@@ -1,41 +1,36 @@
 import React from "react";
-import {shallow} from 'enzyme'
+import { shallow } from "enzyme";
 import Header from "./Header";
-import {findByTestAttr} from "../../utils/testing";
+import { findByTestAttr } from "../../utils/testing";
 import toJson from "enzyme-to-json";
 
-const setUp = (props={}) => {
+const setUp = (props = {}) => {
   return shallow(<Header {...props} />);
 };
 
-describe('Header Component', () => {
-
-  describe('When has props', () => {
+describe("Header Component", () => {
+  describe("When has props", () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = setUp({ appName: "test"} )
+      wrapper = setUp({ appName: "test" });
     });
     afterEach(() => {
       wrapper.unmount();
     });
-    it('Should renderer without errors', () => {
-      const component = findByTestAttr(wrapper, 'HeaderComponent');
+    it("Should renderer without errors", () => {
+      const component = findByTestAttr(wrapper, "HeaderComponent");
       expect(component.length).toBe(1);
     });
-    it('Should have a title', () => {
-      const title = findByTestAttr(wrapper, 'title');
-      expect(title.text()).toBeTruthy();
-    })
   });
 
-  describe('When has NO props', () => {
+  describe("When has NO props", () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = setUp()
+      wrapper = setUp();
     });
     afterEach(() => {
       wrapper.unmount();
     });
-    it('Should not render', () => {}).todo();
+    it("Should not render", () => {}).todo();
   });
 });
