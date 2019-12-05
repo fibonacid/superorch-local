14ccf27426f33f1cbed04bb45300d692ff07e93d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-05 13:54:48 +0100

    CHANGED: remove all sagas

--- src/react/saga/rootSaga.js
+++ src/react/saga/rootSaga.js
@@ -1,18 +1,5 @@
 import { all } from "redux-saga/effects";
-import { receiveMessageWatcher } from "./receiveMessage";
-import { sendDocumentWatcher } from "./sendDocument";
-import { sendSCLangQueryWatcher } from "./sendSCLangQuery";
-import { executeSCLangQueryWatcher } from "./executeSCLangQuery";
-import { sendUserUpdateWatcher } from "./sendUserUpdate";
-import { userAcceptedWatcher } from "./userAccepted";
 
 export function* rootSaga() {
-  yield all([
-    userAcceptedWatcher(),
-    executeSCLangQueryWatcher(),
-    sendUserUpdateWatcher(),
-    sendDocumentWatcher(),
-    sendSCLangQueryWatcher(),
-    receiveMessageWatcher()
-  ]);
+  yield all([]);
 }
