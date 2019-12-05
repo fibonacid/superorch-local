abd850fa75ce1a4900e376e2039084b0e8890836
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-05 14:02:05 +0100

    CHANGED: rename rootSaga to just root

--- src/react/saga/root.js
+++ src/react/saga/root.js
@@ -1,5 +1,5 @@
 import { all } from "redux-saga/effects";
 
-export function* rootSaga() {
+export function* root() {
   yield all([]);
 }
--- src/react/store.js
+++ src/react/store.js
@@ -1,8 +1,8 @@
-import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducers/root'
-import createSagaMiddleware from 'redux-saga';
-import {rootSaga} from "./saga/rootSaga";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/root";
+import createSagaMiddleware from "redux-saga";
+import { root } from "./saga/rootSaga";
 import reduxWebsocket from "@giantmachines/redux-websocket";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -10,19 +10,15 @@ const sagaMiddleware = createSagaMiddleware();
 const reduxWebsocketMiddleware = reduxWebsocket({});
 
 export default function configureStore(preloadedState) {
+  const middlewares = [reduxWebsocketMiddleware, sagaMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-    const middlewares = [
-        reduxWebsocketMiddleware,
-        sagaMiddleware
-    ];
-    const middlewareEnhancer = applyMiddleware(...middlewares);
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
 
-    const enhancers = [middlewareEnhancer];
-    const composedEnhancers = composeWithDevTools(...enhancers);
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
 
-    const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  sagaMiddleware.run(root);
 
-    sagaMiddleware.run(rootSaga);
-
-    return store
+  return store;
 }
