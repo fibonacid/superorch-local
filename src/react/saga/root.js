8e3dce21d123fd87029d2e9154e4328b754a60c1
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-05 22:28:07 +0100

    ADDED: watcher saga for action WS_CREATE_USER

--- src/react/saga/root.js
+++ src/react/saga/root.js
@@ -2,7 +2,13 @@ import { all } from "redux-saga/effects";
 import { wsOpenWatcher } from "./ws/open";
 import { wsMessageWatcher } from "./ws/message";
 import { wsGetUserListWatcher } from "./ws/getUserList";
+import { wsCreateUserWatcher } from "./ws/createUser";
 
 export function* root() {
-  yield all([wsOpenWatcher(), wsMessageWatcher(), wsGetUserListWatcher()]);
+  yield all([
+    wsOpenWatcher(),
+    wsMessageWatcher(),
+    wsGetUserListWatcher(),
+    wsCreateUserWatcher()
+  ]);
 }
---
+++ src/react/saga/ws/createUser.js
@@ -0,0 +1,51 @@
+import { actionTypes } from "../../actions/actionTypes";
+import { takeLatest, put, race, take, delay, select } from "redux-saga/effects";
+import { send } from "@giantmachines/redux-websocket";
+import {
+  wsCreateUser,
+  wsCreateUserError,
+  wsCreateUserSuccess,
+  wsCreateUserTimeout
+} from "../../actions/ws/createUser";
+import { updateUser } from "../../actions/updateUser";
+import { updateMyUserId } from "../../actions/updateMyUserId";
+import { wsGetUserList } from "../../actions/ws/getUserList";
+
+export function* wsCreateUserWatcher() {
+  yield takeLatest(actionTypes.WS_CREATE_USER, wsCreateUserSaga);
+}
+
+export function* wsCreateUserSaga(action) {
+  // Request a new user list
+  yield put(send(wsCreateUser(action.user)));
+
+  // Wait for an error or success message
+  const { result, error, timeout } = yield race({
+    result: take(actionTypes.WS_CREATE_USER_SUCCESS),
+    error: take(actionTypes.WS_CREATE_USER_ERROR),
+    timeout: delay(2000, `Request took too long to complete`)
+  });
+
+  // If request completed successfully:
+  if (result) {
+    // Get default user id
+    const { myUserId } = yield select(state => state.base);
+    // Swap it with new one:
+    // Update the record in the users reducer.
+    yield put(updateUser(myUserId, { id: result.userId }));
+    // Update the record inside base reducer.
+    yield put(updateMyUserId(result.userId));
+    // Finally ask for a new user list
+    yield put(wsGetUserList());
+  }
+  // If request raised an error on the server:
+  else if (error) {
+    console.error(error);
+    yield put(wsCreateUserError(error));
+  }
+  // If request took to long to complete
+  else if (timeout) {
+    console.log(timeout);
+    yield put(wsCreateUserTimeout(timeout));
+  }
+}
