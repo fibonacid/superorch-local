480f2f8a1b9fb02e6a528e3b3b0d75160ae3ddc8
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-05 17:43:10 +0100

    ADDED: watcher saga for swapUserId

--- src/react/saga/root.js
+++ src/react/saga/root.js
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
 import { wsOpenWatcher } from "./ws/open";
 import { wsMessageWatcher } from "./ws/message";
+import { swapUserIdWatcher } from "./swapUserId";
 
 export function* root() {
-  yield all([wsOpenWatcher(), wsMessageWatcher()]);
+  yield all([wsOpenWatcher(), wsMessageWatcher(), swapUserIdWatcher()]);
 }
--- src/react/saga/ws/swapUserId.js
+++
@@ -1,9 +0,0 @@
-import { select, put } from "redux-saga/effects";
-import { updateUser } from "../../actions/updateUser";
-import { selectDefaultUser } from "../../reducers/root";
-
-export function* swapUserId(newId) {
-  const { id } = yield select(state => selectDefaultUser(state));
-
-  yield put(updateUser(id, { id: newId }));
-}
