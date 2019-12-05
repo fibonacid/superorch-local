32f9087bddcba58ca5287f04db038ae7a45dd719
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-05 14:24:27 +0100

    FIXED: change outdated api

--- src/react/reducers/websocket.js
+++ src/react/reducers/websocket.js
@@ -11,25 +11,25 @@ const initialState = {
 
 const websocket = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.WEBSOCKET_OPEN:
+    case actionTypes.WS_OPEN:
       return {
         ...state,
         isConnected: true,
         isTryingToConnect: false
       };
-    case actionTypes.WEBSOCKET_CLOSED:
+    case actionTypes.WS_CLOSED:
       return {
         ...state,
         isConnected: false,
         isTryingToConnect: false
       };
-    case actionTypes.WEBSOCKET_CONNECT:
+    case actionTypes.WS_CONNECT:
       return {
         ...state,
         url: action.payload.url,
         isTryingToConnect: true
       };
-    case actionTypes.WEBSOCKET_BEGIN_RECONNECT:
+    case actionTypes.WS_BEGIN_RECONNECT:
       return {
         ...state,
         isTryingToConnect: true
