0fe3a816055e41bc01627ef25c09049ae25ef119
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-07 20:07:00 +0100

    ADDED: ws actions tests

---
+++ src/react/tests/actions/ws/userUpdate.test.js
@@ -0,0 +1,14 @@
+import { actionTypes } from "../../../actions/actionTypes";
+import { wsUserUpdate } from "../../../actions/ws/userUpdate";
+
+describe("wsUserUpdate action", () => {
+  it("should create an action to send a user update to the server", () => {
+    const userId = 0;
+    const diff = {};
+    expect(wsUserUpdate(userId, diff)).toEqual({
+      type: actionTypes.WS_USER_UPDATE,
+      userId,
+      diff
+    });
+  });
+});
