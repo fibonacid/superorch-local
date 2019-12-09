02a93486aeb2d62870e7f11313107f1065bc563d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-09 23:26:30 +0100

    ADDED: select login user through myUserId value

--- src/react/saga/client/loginRequest.js
+++ src/react/saga/client/loginRequest.js
@@ -1,11 +1,20 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest, put } from "redux-saga/effects";
+import { takeLatest, put, select } from "redux-saga/effects";
 import { send } from "@giantmachines/redux-websocket";
+import { selectUser } from "../../reducers/root";
 
 export function* c_loginRequestWatcher() {
   yield takeLatest(actionTypes.C_LOGIN_REQUEST, c_loginRequestSaga);
 }
 
 export function* c_loginRequestSaga(action) {
-  yield put(send(action));
+  const { myUserId } = yield select(state => state.base);
+  const userData = yield select(state => selectUser(state, myUserId));
+
+  const message = {
+    ...action,
+    userData
+  };
+
+  yield put(send(message));
 }
