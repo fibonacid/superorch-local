65c7b8d82935543e2134436def49aaa8b076f4b4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-09 23:44:39 +0100

    ADDED: update client with new associated userId value

--- src/react/saga/server/createUserSaga.js
+++ src/react/saga/server/createUserSaga.js
@@ -1,5 +1,6 @@
 import { put } from "redux-saga/effects";
 import { s_createUser } from "../../actions/server/crudUsers";
+import { s_updateClient } from "../../actions/server/crudClients";
 
 let userCount = 0;
 
@@ -17,7 +18,16 @@ export function* s_createUserSaga(userData, clientId) {
   try {
     // Store new user
     yield put(s_createUser(newUser.id, newUser));
+    // Assign user to it's parent client
+    yield put(s_updateClient(clientId, { userId: newUser.id }));
+
+    // If everything is alright transmit the userId to the newly connected client.
+    //yield put(wsTransmitMessage(wsCreateUserSuccess(newUser.id), clientId));
+    // Then, broadcast new user to all the other clients
+    //yield put(wsBroadcastMessage(wsUserJoined(newUser), clientId));
   } catch (error) {
-    console.error(error);
+    // If there was an error:
+    // Transmit error to the newly connected client.
+    //yield put(wsTransmitMessage(wsCreateUserError(error), clientId));
   }
 }
