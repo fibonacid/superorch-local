4e7d31003f0f25c41d252d449c13942fc3dd5acc
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-09 23:27:01 +0100

    ADDED: create user with generated id and received data

--- src/react/saga/server/createUserSaga.js
+++ src/react/saga/server/createUserSaga.js
@@ -1,3 +1,23 @@
+import { put } from "redux-saga/effects";
+import { s_createUser } from "../../actions/server/crudUsers";
+
+let userCount = 0;
+
 export function* s_createUserSaga(userData, clientId) {
-  console.log("s_createUserSaga", userData, clientId);
+  // Update user count
+  userCount++;
+
+  // Create new user with given data,
+  // but locally generated id.
+  const newUser = {
+    ...userData,
+    id: userCount
+  };
+
+  try {
+    // Store new user
+    yield put(s_createUser(newUser.id, newUser));
+  } catch (error) {
+    console.error(error);
+  }
 }
