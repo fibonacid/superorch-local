4a467c594f177f068318d5c8a7f59c396cccd92e
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-09 20:37:56 +0100

    ADDED: websocket channels

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,10 @@
 module.exports = {
   channels: {
     APP_INFO: "app_info",
-    START_SUPERCOLLIDER: "start_supercollider"
+    START_SUPERCOLLIDER: "start_supercollider",
+    START_WS_SERVER: "start_ws_server",
+    WEBSOCKET_OPEN: "websocket_open",
+    WEBSOCKET_CLOSED: "websocket_closed",
+    WEBSOCKET_MESSAGE: "websocket_message"
   }
 };
