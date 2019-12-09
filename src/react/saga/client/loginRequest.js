e8191b594d53244021f8ed70d3bbb4e34ce763cf
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-09 22:12:21 +0100

    ADDED: saga to send a login request

--- src/react/saga/client/loginRequest.js
+++ src/react/saga/client/loginRequest.js
@@ -1,10 +1,11 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, put } from "redux-saga/effects";
+import { send } from "@giantmachines/redux-websocket";
 
 export function* c_loginRequestWatcher() {
   yield takeLatest(actionTypes.C_LOGIN_REQUEST, c_loginRequestSaga);
 }
 
 export function* c_loginRequestSaga(action) {
-  console.log("c_loginRequestSaga");
+  yield put(send(action));
 }
