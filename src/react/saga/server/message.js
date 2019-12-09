daba75f83feb994c0b81b686121d9464e9243429
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-09 22:36:42 +0100

    ADDED: handle message C_LOGIN_REQUEST

--- src/react/saga/server/message.js
+++ src/react/saga/server/message.js
@@ -1,5 +1,6 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, call } from "redux-saga/effects";
+import { s_createUserSaga } from "./createUserSaga";
 
 export function* s_messageWatcher() {
   yield takeLatest(actionTypes.S_MESSAGE, s_messageSaga);
@@ -7,4 +8,13 @@ export function* s_messageWatcher() {
 
 export function* s_messageSaga(action) {
   console.log("s_messageSaga", action);
+  // Unpack message
+  const { clientId } = action;
+  const message = JSON.parse(action.message);
+
+  // Handle actions embedded in the message
+  switch (message.type) {
+    case actionTypes.C_LOGIN_REQUEST:
+      return yield call(s_createUserSaga, message.userData, clientId);
+  }
 }
