aa833b84312d1e046039c6494d87f8408f57f3a8
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 18:37:13 +0100

    ADDED: parse message

--- src/react/saga/client/message.js
+++ src/react/saga/client/message.js
@@ -1,12 +1,19 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, put } from "redux-saga/effects";
+import { s_loginSuccess } from "../../actions/server/loginResponse";
 
 export function* c_messageWatcher() {
   yield takeLatest(actionTypes.C_MESSAGE, c_messageSaga);
 }
 
-export function* c_messageSaga(action) {
-  console.log("c_messageSaga", {
-    message: { ...action }
-  });
+export function* c_messageSaga({ payload }) {
+  // Unpack message
+  const message = JSON.parse(payload.message);
+
+  //console.log(message);
+
+  switch (message.type) {
+    case actionTypes.S_LOGIN_SUCCESS:
+      yield put(s_loginSuccess(message.userId));
+  }
 }
