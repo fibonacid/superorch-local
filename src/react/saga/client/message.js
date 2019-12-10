b08a5fa7bbd9f9deabecee06e47535c5471bd423
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 19:30:30 +0100

    ADDED: message handlers

--- src/react/saga/client/message.js
+++ src/react/saga/client/message.js
@@ -1,6 +1,12 @@
 import { actionTypes } from "../../actions/actionTypes";
 import { takeLatest, put } from "redux-saga/effects";
-import { s_loginSuccess } from "../../actions/server/loginResponse";
+import {
+  s_loginError,
+  s_loginSuccess
+} from "../../actions/server/loginResponse";
+import { b_userJoined } from "../../actions/broadcast/userJoined";
+import { b_userLeft } from "../../actions/broadcast/userLeft";
+import { b_userUpdate } from "../../actions/broadcast/userUpdate";
 
 export function* c_messageWatcher() {
   yield takeLatest(actionTypes.C_MESSAGE, c_messageSaga);
@@ -10,10 +16,23 @@ export function* c_messageSaga({ payload }) {
   // Unpack message
   const message = JSON.parse(payload.message);
 
-  //console.log(message);
+  console.log(`message: ${message.type}`);
 
   switch (message.type) {
     case actionTypes.S_LOGIN_SUCCESS:
       yield put(s_loginSuccess(message.userId));
+      break;
+    case actionTypes.S_LOGIN_ERROR:
+      yield put(s_loginError(message.error));
+      break;
+    case actionTypes.B_USER_JOINED:
+      yield put(b_userJoined(message.userId, message.userData));
+      break;
+    case actionTypes.B_USER_UPDATE:
+      yield put(b_userUpdate(message.userId, message.userData));
+      break;
+    case actionTypes.B_USER_LEFT:
+      yield put(b_userLeft(message.userId));
+      break;
   }
 }
