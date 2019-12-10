77b9f956f5fd2551d261fa34c15e47ca72784375
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 11:22:36 +0100

    ADDED: channels for websocket transmit and broadcast of messages

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -5,6 +5,8 @@ module.exports = {
     START_WS_SERVER: "start_ws_server",
     WEBSOCKET_OPEN: "websocket_open",
     WEBSOCKET_CLOSED: "websocket_closed",
-    WEBSOCKET_MESSAGE: "websocket_message"
+    WEBSOCKET_MESSAGE: "websocket_message",
+    WEBSOCKET_TRANSMIT: "websocket_transmit",
+    WEBSOCKET_BROADCAST: "websocket_broadcast"
   }
 };
