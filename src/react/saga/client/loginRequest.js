44a59602eff3badd27f5363cdedcedd62c62be86
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 19:21:28 +0100

    ADDED: request resolution

--- src/react/saga/client/loginRequest.js
+++ src/react/saga/client/loginRequest.js
@@ -1,7 +1,9 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest, put, select } from "redux-saga/effects";
+import { takeLatest, put, select, race, take, delay } from "redux-saga/effects";
 import { send } from "@giantmachines/redux-websocket";
 import { selectUser } from "../../reducers/root";
+import { c_updateUser } from "../../actions/client/crudUsers";
+import { c_updateMyUserId } from "../../actions/client/updateMyUserId";
 
 export function* c_loginRequestWatcher() {
   yield takeLatest(actionTypes.C_LOGIN_REQUEST, c_loginRequestSaga);
@@ -16,5 +18,26 @@ export function* c_loginRequestSaga(action) {
     userData
   };
 
+  // Send request
   yield put(send(message));
+
+  // start a race between sagas
+  const { result, error, timeout } = yield race({
+    result: take(actionTypes.S_LOGIN_SUCCESS),
+    error: take(actionTypes.S_LOGIN_ERROR),
+    timeout: delay(2000, `Request took too long to complete`)
+  });
+
+  if (result) {
+    // Get default user id
+    const { myUserId } = yield select(state => state.wsclient);
+    // Update default user
+    yield put(c_updateUser(myUserId, { id: result.userId }));
+    // Update myUserId variable
+    yield put(c_updateMyUserId(result.userId));
+  } else if (error) {
+    console.error(error);
+  } else if (timeout) {
+    console.error(timeout);
+  }
 }
