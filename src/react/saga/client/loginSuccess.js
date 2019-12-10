ddf16909be74bce5872909bd10e78ae2638f6818
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 23:29:18 +0100

    ADDED: dispatch action C_GET_USER_LIST_REQUEST

--- src/react/saga/client/loginSuccess.js
+++ src/react/saga/client/loginSuccess.js
@@ -2,6 +2,7 @@ import { put, select, takeLatest } from "redux-saga/effects";
 import { actionTypes } from "../../actions/actionTypes";
 import { c_updateUser } from "../../actions/client/crudUsers";
 import { c_updateMyUserId } from "../../actions/client/updateMyUserId";
+import { c_getUserListRequest } from "../../actions/client/getUserListRequest";
 
 export function* c_loginSuccessWatcher() {
   yield takeLatest(actionTypes.C_LOGIN_SUCCESS, c_loginSuccessSaga);
@@ -15,4 +16,7 @@ export function* c_loginSuccessSaga(action) {
   yield put(c_updateUser(myUserId, { id: action.userId }));
   // Update myUserId variable
   yield put(c_updateMyUserId(action.userId));
+
+  // Then request a user list
+  yield put(c_getUserListRequest());
 }
