d831bea5e498f4dfa3cccac76c574e7386e0fa98
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 10:35:55 +0100

    ADDED: transmit and broadcast data

--- src/react/saga/server/loginResponse.js
+++ src/react/saga/server/loginResponse.js
@@ -1,7 +1,11 @@
-import { put, select } from "redux-saga/effects";
-import { s_createUser, s_updateUser } from "../../actions/server/crudUsers";
-import { s_updateClient } from "../../actions/server/crudClients";
-import { selectUser } from "../../reducers/root";
+import { put } from "redux-saga/effects";
+import { s_transmit } from "../../actions/server/transmit";
+import {
+  s_loginError,
+  s_loginSuccess
+} from "../../actions/server/loginResponse";
+import { s_broadcast } from "../../actions/server/broadcast";
+import { b_userJoined } from "../../actions/broadcast/userJoined";
 
 let userCount = 0;
 
@@ -16,31 +20,15 @@ export function* s_loginResponseSaga(userData, clientId) {
     id: userCount
   };
 
-  console.log(userData);
-
   try {
-    // Find out if user already exists in the store:
-    /*const user = yield select(state => selectUser(state, userData.id));
-    // If user already exists:
-    if (user) {
-      // Assign him a new id.
-      yield put(s_updateUser(user.id, newUser));
-    }
-    // If user doesn't exist yet:
-    else {
-      // Create it.
-      yield put(s_createUser(newUser.id, newUser));
-    }()*/
-    //yield put(s_createUser(newUser.id, newUser));
-    // Finally, assign user to its parent client.
-    //yield put(s_updateClient(clientId, { userId: newUser.id }));
-    // If everything is alright transmit the userId to the newly connected client.
-    //yield put(wsTransmitMessage(wsCreateUserSuccess(newUser.id), clientId));
-    // Then, broadcast new user to all the other clients
-    //yield put(wsBroadcastMessage(wsUserJoined(newUser), clientId));
+    // Transmit new user id to the client that tried logging in.
+    yield put(s_transmit(clientId, s_loginSuccess(newUser.id)));
+
+    // Broadcast new user to the other clients
+    yield put(s_broadcast(clientId, b_userJoined(newUser.id, newUser)));
   } catch (error) {
     // If there was an error:
-    // Transmit error to the newly connected client.
-    //yield put(wsTransmitMessage(wsCreateUserError(error), clientId));
+    // Transmit error to the client that tried logging in.
+    yield put(s_transmit(clientId, s_loginError(error)));
   }
 }
