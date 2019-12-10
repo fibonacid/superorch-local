1eb66d8d446c28e2e64e11ca9964bfcf01a4adbd
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 23:45:17 +0100

    ADDED: saga to handle C_GET_USER_LIST_REQUEST

--- src/react/saga/client/getUserListRequest.js
+++ src/react/saga/client/getUserListRequest.js
@@ -1,5 +1,12 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { delay, put, race, take, takeLatest } from "redux-saga/effects";
+import { send } from "@giantmachines/redux-websocket/dist";
+import {
+  c_getUserListError,
+  c_getUserListRequest,
+  c_getUserListSuccess,
+  c_getUserListTimeout
+} from "../../actions/client/getUserListRequest";
 
 export function* c_getUserListRequestWatcher() {
   yield takeLatest(
@@ -8,4 +15,22 @@ export function* c_getUserListRequestWatcher() {
   );
 }
 
-export function* c_getUserListRequestSaga(action) {}
+export function* c_getUserListRequestSaga(action) {
+  // Send request
+  yield put(send(c_getUserListRequest()));
+
+  // start a race between sagas
+  const { result, error, timeout } = yield race({
+    result: take(actionTypes.S_GET_USER_LIST_SUCCESS),
+    error: take(actionTypes.S_GET_USER_LIST_ERROR),
+    timeout: delay(2000, `Request took too long to complete`)
+  });
+
+  if (result) {
+    yield put(c_getUserListSuccess(result.userList, `user list received`));
+  } else if (error) {
+    yield put(c_getUserListError(error));
+  } else if (timeout) {
+    yield put(c_getUserListTimeout(timeout));
+  }
+}
