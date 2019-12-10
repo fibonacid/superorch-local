c99eb1e20a6626c3f06c493b18779f13e9583e54
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 22:15:33 +0100

    ADDED: handler for message C_LOGOUT_REQUEST

--- src/react/saga/server/message.js
+++ src/react/saga/server/message.js
@@ -1,13 +1,13 @@
 import { actionTypes } from "../../actions/actionTypes";
 import { takeLatest, call } from "redux-saga/effects";
 import { s_loginResponseSaga } from "./loginResponse";
+import { s_logoutResponseSaga } from "./logoutResponse";
 
 export function* s_messageWatcher() {
   yield takeLatest(actionTypes.S_MESSAGE, s_messageSaga);
 }
 
 export function* s_messageSaga(action) {
-  console.log("s_messageSaga", action);
   // Unpack message
   const { clientId } = action;
   const message = JSON.parse(action.message);
@@ -18,5 +18,7 @@ export function* s_messageSaga(action) {
   switch (message.type) {
     case actionTypes.C_LOGIN_REQUEST:
       return yield call(s_loginResponseSaga, message.userData, clientId);
+    case actionTypes.C_LOGOUT_REQUEST:
+      return yield call(s_logoutResponseSaga, clientId);
   }
 }
