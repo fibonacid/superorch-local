ee6bd80a372a53bc7e90abdaa76e66c5282d0cca
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 20:49:25 +0100

    ADDED: handlers for messages S_LOGOUT_SUCCESS and S_LOGOUT_ERROR

--- src/react/saga/client/message.js
+++ src/react/saga/client/message.js
@@ -7,6 +7,10 @@ import {
 import { b_userJoined } from "../../actions/broadcast/userJoined";
 import { b_userLeft } from "../../actions/broadcast/userLeft";
 import { b_userUpdate } from "../../actions/broadcast/userUpdate";
+import {
+  s_logoutError,
+  s_logoutSuccess
+} from "../../actions/server/logoutReponse";
 
 export function* c_messageWatcher() {
   yield takeLatest(actionTypes.C_MESSAGE, c_messageSaga);
@@ -24,6 +28,12 @@ export function* c_messageSaga({ payload }) {
       break;
     case actionTypes.S_LOGIN_ERROR:
       yield put(s_loginError(message.error));
+      break;
+    case actionTypes.S_LOGOUT_SUCCESS:
+      yield put(s_logoutSuccess());
+      break;
+    case actionTypes.S_LOGOUT_ERROR:
+      yield put(s_logoutError(message.error));
       break;
     case actionTypes.B_USER_JOINED:
       yield put(b_userJoined(message.userId, message.userData));
