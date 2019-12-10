da7ec095335303610d7fab16225298f425a494a1
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-10 23:35:24 +0100

    ADDED: action S_GET_USER_LIST_SUCCESS and S_GET_USER_LIST_ERROR

---
+++ src/react/actions/server/getUserListResponse.js
@@ -0,0 +1,11 @@
+import { actionTypes } from "../actionTypes";
+
+export const s_getUserListSuccess = userList => ({
+  type: actionTypes.S_GET_USER_LIST_SUCCESS,
+  userList
+});
+
+export const s_getUserListError = error => ({
+  type: actionTypes.S_GET_USER_LIST_ERROR,
+  error
+});
---
+++ src/react/tests/actions/server/getUserListResponse.test.js
@@ -0,0 +1,25 @@
+import { actionTypes } from "../../../actions/actionTypes";
+import {
+  s_getUserListSuccess,
+  s_getUserListError
+} from "../../../actions/server/getUserListResponse";
+
+describe("s_getUserListSuccess action", () => {
+  it("should create an action to communicate that the request completed successfully", () => {
+    const userList = {};
+    expect(s_getUserListSuccess(userList)).toEqual({
+      type: actionTypes.S_GET_USER_LIST_SUCCESS,
+      userList
+    });
+  });
+});
+
+describe("s_getUserListError action", () => {
+  it("should create an action to communicate that the request raised some errors", () => {
+    const error = new Error();
+    expect(s_getUserListError(error)).toEqual({
+      type: actionTypes.S_GET_USER_LIST_ERROR,
+      error
+    });
+  });
+});
