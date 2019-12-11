e251c44ea9baf2a4683dbb3deb6ddbbb8949eea2
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-11 14:49:20 +0100

    ADDED: request handler saga for C_UPDATE_USER_DATA_SUCCESS

--- src/react/saga/client/requests/updateUserDataRequest.js
+++ src/react/saga/client/requests/updateUserDataRequest.js
@@ -1,5 +1,12 @@
 import { actionTypes } from "../../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, put, race, delay, take } from "redux-saga/effects";
+import { send } from "@giantmachines/redux-websocket";
+import {
+  c_updateUserDataError,
+  c_updateUserDataRequest,
+  c_updateUserDataSuccess,
+  c_updateUserDataTimeout
+} from "../../../actions/client/requests/updateUserDataRequest";
 
 export function* c_updateUserDataRequestWatcher() {
   yield takeLatest(
@@ -9,5 +16,21 @@ export function* c_updateUserDataRequestWatcher() {
 }
 
 export function* c_updateUserDataRequestSaga(action) {
-  console.log("c_updateUserDataRequestSaga", action);
+  // Send request:
+  yield put(send(c_updateUserDataRequest(action.userData)));
+
+  // start a race between sagas
+  const { result, error, timeout } = yield race({
+    result: take(actionTypes.S_UPDATE_USER_DATA_SUCCESS),
+    error: take(actionTypes.S_UPDATE_USER_DATA_ERROR),
+    timeout: delay(2000, `Request took too long to complete`)
+  });
+
+  if (result) {
+    yield put(c_updateUserDataSuccess(`user updated`));
+  } else if (error) {
+    yield put(c_updateUserDataError(error));
+  } else if (timeout) {
+    yield put(c_updateUserDataTimeout(timeout));
+  }
 }
