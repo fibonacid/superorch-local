8cffdd3c57cd720da035fbd89064560d8c508852
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-11 14:41:57 +0100

    ADDED: check conditions and then dispatch action C_UPDATE_USER_DATA_REQUEST

--- src/react/saga/client/updateUser.js
+++ src/react/saga/client/updateUser.js
@@ -1,10 +1,16 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest } from "@redux-saga/core/effects";
+import { takeLatest, put } from "redux-saga/effects";
+import { select } from "redux-saga/effects";
+import { c_updateUserDataRequest } from "../../actions/client/requests/updateUserDataRequest";
 
 export function* c_updateUserWatcher() {
   yield takeLatest(actionTypes.C_UPDATE_USER, c_updateUserSaga);
 }
 
 export function* c_updateUserSaga(action) {
-  console.log("c_updateUserSaga", action);
+  const { myUserId, isLoggedIn } = yield select(state => state.wsclient);
+  // if client is logged in and the user update involves the default user:
+  if (isLoggedIn && action.userId === myUserId) {
+    yield put(c_updateUserDataRequest(action.userData));
+  }
 }
