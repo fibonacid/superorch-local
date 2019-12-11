659bf2179119099e35c80a4d1aa7ed66f1b69a11
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-11 17:52:45 +0100

    ADDED: ui to add a new document

--- src/react/components/Tmp.js
+++ src/react/components/Tmp.js
@@ -3,9 +3,14 @@ import { connect } from "react-redux";
 import { c_loginRequest } from "../actions/client/requests/loginRequest";
 import { c_logoutRequest } from "../actions/client/requests/logoutRequest";
 import { c_updateUser } from "../actions/client/crudUsers";
+import {
+  c_appendDocument,
+  c_createDocument
+} from "../actions/client/crudDocuments";
 
 function Tmp(props) {
   const [userName, setUserName] = useState("");
+  const [docValue, setDocValue] = useState("");
 
   return (
     <div>
@@ -16,10 +21,17 @@ function Tmp(props) {
         logout
       </button>
       <div>
-        <input value={userName} onChange={e => setUserName(e.target.value)} />
         <label>username</label>
+        <input value={userName} onChange={e => setUserName(e.target.value)} />
         <button onClick={() => props.updateUsername(props.myUserId, userName)}>
           submit
+        </button>
+      </div>
+      <div>
+        <label>document</label>
+        <input value={docValue} onChange={e => setDocValue(e.target.value)} />
+        <button onClick={() => props.appendDocument(docValue)}>
+          create document
         </button>
       </div>
     </div>
@@ -29,12 +41,14 @@ function Tmp(props) {
 const mapDispatchToProps = dispatch => ({
   login: () => dispatch(c_loginRequest()),
   logout: () => dispatch(c_logoutRequest()),
-  updateUsername: (userId, name) => dispatch(c_updateUser(userId, { name }))
+  updateUsername: (userId, name) => dispatch(c_updateUser(userId, { name })),
+  appendDocument: value => dispatch(c_appendDocument(value))
 });
 
 const mapStateToProps = state => ({
   isLoggedIn: state.wsclient.isLoggedIn,
-  myUserId: state.wsclient.myUserId
+  myUserId: state.wsclient.myUserId,
+  myDocIds: state.wsclient.myDocIds
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tmp);
