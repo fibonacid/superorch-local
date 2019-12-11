8ed15278269711147fa24b60341e38c9b30d4efc
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-11 21:38:44 +0100

    ADDED: saga to handle request C_CREATE_DOCUMENT

--- src/react/saga/client/requests/createDocumentRequest.js
+++ src/react/saga/client/requests/createDocumentRequest.js
@@ -1,5 +1,12 @@
 import { actionTypes } from "../../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, put, race, take, delay } from "redux-saga/effects";
+import { send } from "@giantmachines/redux-websocket";
+import {
+  c_createDocumentError,
+  c_createDocumentRequest,
+  c_createDocumentSuccess,
+  c_createDocumentTimeout
+} from "../../../actions/client/requests/createDocumentRequest";
 
 export function* c_createDocumentRequestWatcher() {
   yield takeLatest(
@@ -10,4 +17,22 @@ export function* c_createDocumentRequestWatcher() {
 
 export function* c_createDocumentRequestSaga(action) {
   console.log("c_createDocumentRequestSaga", action);
+
+  // Send request
+  yield put(send(c_createDocumentRequest(action.docData)));
+
+  // start a race between sagas
+  const { result, error, timeout } = yield race({
+    result: take(actionTypes.S_CREATE_DOCUMENT_SUCCESS),
+    error: take(actionTypes.S_CREATE_DOCUMENT_ERROR),
+    timeout: delay(2000, `Request took too long to complete`)
+  });
+
+  if (result) {
+    yield put(c_createDocumentSuccess(result.userId, `logged in`));
+  } else if (error) {
+    yield put(c_createDocumentError(error));
+  } else if (timeout) {
+    yield put(c_createDocumentTimeout(timeout));
+  }
 }
