02a4b234e60d40bb95cb78c7cc0195e49dc4ddc9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-12 12:36:06 +0100

    ADDED: on success dispatch a C_CREATE_DOCUMENT_REQUEST for each document possessed by the client

--- src/react/saga/client/requests/loginSuccess.js
+++ src/react/saga/client/requests/loginSuccess.js
@@ -1,8 +1,10 @@
-import { put, select, takeLatest } from "redux-saga/effects";
+import { put, select, takeLatest, all } from "redux-saga/effects";
 import { actionTypes } from "../../../actions/actionTypes";
 import { c_updateUser } from "../../../actions/client/crudUsers";
 import { c_updateMyUserId } from "../../../actions/client/updateMyUserId";
 import { c_getUserListRequest } from "../../../actions/client/requests/getUserListRequest";
+import { c_createDocumentRequest } from "../../../actions/client/requests/createDocumentRequest";
+import { selectDocuments } from "../../../reducers/root";
 
 export function* c_loginSuccessWatcher() {
   yield takeLatest(actionTypes.C_LOGIN_SUCCESS, c_loginSuccessSaga);
@@ -17,6 +19,17 @@ export function* c_loginSuccessSaga(action) {
   // Update myUserId variable
   yield put(c_updateMyUserId(action.userId));
 
-  // Then request a user list
+  // Request a user list
   yield put(c_getUserListRequest());
+
+  // Send my documents
+  const { myDocIds } = yield select(state => state.wsclient);
+  const documents = yield select(state => selectDocuments(state));
+
+  yield all(
+    documents
+      // Filter out documents owned by other users
+      .filter(document => myDocIds.indexOf(document.id) !== -1)
+      .map(document => put(c_createDocumentRequest(document)))
+  );
 }
