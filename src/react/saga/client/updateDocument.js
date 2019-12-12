334607a3bc63e006109b22a4a8c7b6ae3346a170
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-12 15:25:36 +0100

    CHANGED: use redux-saga effect debounce instead of takeLatest

--- src/react/saga/client/updateDocument.js
+++ src/react/saga/client/updateDocument.js
@@ -1,10 +1,10 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { put, select, takeLatest } from "redux-saga/effects";
+import { put, select, debounce } from "redux-saga/effects";
 import { selectDocument } from "../../reducers/root";
 import { c_updateDocumentDataRequest } from "../../actions/client/requests/updateDocumentDataRequest";
 
 export function* c_updateDocumentWatcher() {
-  yield takeLatest(actionTypes.C_UPDATE_DOCUMENT, c_updateDocumentSaga);
+  yield debounce(50, actionTypes.C_UPDATE_DOCUMENT, c_updateDocumentSaga);
 }
 
 export function* c_updateDocumentSaga(action) {
