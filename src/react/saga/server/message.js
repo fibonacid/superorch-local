28debdba9fd38d5d8e8f6b8718ca47dc78b4dfa2
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-12 11:15:20 +0100

    ADDED: transmit error message when something goes wrong

--- src/react/saga/server/message.js
+++ src/react/saga/server/message.js
@@ -1,41 +1,63 @@
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest, call } from "redux-saga/effects";
+import { takeLatest, put, select, call } from "redux-saga/effects";
 import { s_loginResponseSaga } from "./responses/loginResponse";
 import { s_logoutResponseSaga } from "./responses/logoutResponse";
 import { s_getUserListResponseSaga } from "./responses/getUserListResponse";
 import { s_updateUserDataResponseSaga } from "./responses/updateUserDataResponse";
 import { s_createDocumentResponseSaga } from "./responses/createDocumentResponse";
+import { selectClient, selectUser } from "../../reducers/root";
+import { s_transmit } from "../../actions/server/transmit";
+import { s_messageError } from "../../actions/server/message";
+import { statusCodes } from "../../utils/constants";
 
 export function* s_messageWatcher() {
   yield takeLatest(actionTypes.S_MESSAGE, s_messageSaga);
 }
 
 export function* s_messageSaga(action) {
-  // Unpack message
-  const { clientId } = action;
-  const message = JSON.parse(action.message);
+  try {
+    // Unpack message
+    const { clientId } = action;
+    const message = JSON.parse(action.message);
 
-  console.log(`server received a message: ${message.type}`);
+    console.log(`server received a message: ${message.type}`);
 
-  // Handle actions embedded in the message
-  switch (message.type) {
-    case actionTypes.C_LOGIN_REQUEST:
-      return yield call(s_loginResponseSaga, clientId, message.userData);
-    case actionTypes.C_LOGOUT_REQUEST:
-      return yield call(s_logoutResponseSaga, clientId);
-    case actionTypes.C_GET_USER_LIST_REQUEST:
-      return yield call(s_getUserListResponseSaga, clientId);
-    case actionTypes.C_UPDATE_USER_DATA_REQUEST:
-      return yield call(
-        s_updateUserDataResponseSaga,
-        clientId,
-        message.userData
+    // Get user associated with the client
+    const { userId } = yield select(state => selectClient(state, clientId));
+    const user = yield select(state => selectUser(state, userId));
+
+    // If user is undefined then communication should be interrupted
+    if (!user) {
+      return yield put(
+        s_transmit(clientId, s_messageError(400, `Log in is required`))
       );
-    case actionTypes.C_CREATE_DOCUMENT_REQUEST:
-      return yield call(
-        s_createDocumentResponseSaga,
-        clientId,
-        message.docData
-      );
+    }
+
+    // Handle actions embedded in the message
+    switch (message.type) {
+      case actionTypes.C_LOGIN_REQUEST:
+        return yield call(s_loginResponseSaga, clientId, message.userData);
+      case actionTypes.C_LOGOUT_REQUEST:
+        return yield call(s_logoutResponseSaga, clientId);
+      case actionTypes.C_GET_USER_LIST_REQUEST:
+        return yield call(s_getUserListResponseSaga, clientId);
+      case actionTypes.C_UPDATE_USER_DATA_REQUEST:
+        return yield call(
+          s_updateUserDataResponseSaga,
+          clientId,
+          message.userData
+        );
+      case actionTypes.C_CREATE_DOCUMENT_REQUEST:
+        return yield call(
+          s_createDocumentResponseSaga,
+          clientId,
+          message.docData
+        );
+    }
+  } catch (error) {
+    console.error(error);
+    yield put(
+      s_transmit(action.clientId, s_messageError(500, statusCodes[500]))
+    );
   }
 }
