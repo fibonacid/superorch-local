012a8ea98ce9872b4b8897af136663a6402ed8d0
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-13 12:09:23 +0100

    ADDED: calculate id and dispatch action C_CREATE_SC_QUERY

--- src/react/saga/client/appendScQuery.js
+++ src/react/saga/client/appendScQuery.js
@@ -1,5 +1,9 @@
+import _ from "lodash";
 import { actionTypes } from "../../actions/actionTypes";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, select, put } from "redux-saga/effects";
+import { selectScQueries } from "../../reducers/client";
+import { c_createScQuery } from "../../actions/client/crudScQueries";
+import { c_createScQueryRequest } from "../../actions/client/requests/createScQueryRequest";
 
 export function* c_appendScQueryWatcher() {
   yield takeLatest(actionTypes.C_APPEND_SC_QUERY, c_appendScQuerySaga);
@@ -7,4 +11,24 @@ export function* c_appendScQueryWatcher() {
 
 export function* c_appendScQuerySaga(action) {
   console.log("c_appendScQuerySaga", action);
+
+  // Get list of queries
+  const scQueries = yield select(state => selectScQueries(state));
+  // Calculate next id
+  const nextId = 1 + _.maxBy(scQueries, scQuery => scQuery.id);
+
+  // Dispatch an action to create a new scQuery
+  yield put(
+    c_createScQuery(nextId, {
+      ...action.scqData,
+      id: nextId
+    })
+  );
+
+  const { isLoggedIn } = yield select(state => state.client.status);
+
+  // if client is logged in, send a request to the server
+  if (isLoggedIn) {
+    yield put(c_createScQueryRequest(action.userData));
+  }
 }
