d91c9efe4feed4ee445d8be345667c8e7aa7a974
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-13 17:09:02 +0100

    ADDED: dispatch action C_EXEC_SC_CODE_REQUEST

--- src/react/saga/client/createScQuery.js
+++ src/react/saga/client/createScQuery.js
@@ -1,6 +1,7 @@
 import { actionTypes } from "../../actions/actionTypes";
 import { takeEvery, select, put } from "redux-saga/effects";
 import { c_createScQueryRequest } from "../../actions/client/requests/createScQueryRequest";
+import { c_execScCodeRequest } from "../../actions/client/requests/execScCodeRequest";
 
 export function* c_createScQueryWatcher() {
   yield takeEvery(actionTypes.C_CREATE_SC_QUERY, c_createScQuerySaga);
@@ -11,6 +12,10 @@ export function* c_createScQuerySaga(action) {
   const { isLoggedIn, myScQueryIds } = yield select(
     state => state.client.status
   );
+
+  // Dispatch message to supercollider
+  const { input: scCode } = action.scqData;
+  yield put(c_execScCodeRequest(scCode));
 
   // If it's client not logged in:
   if (!isLoggedIn) {
