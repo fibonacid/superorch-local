adef23632b3dad65374b1222d4303e010a44cdb8
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-13 16:38:23 +0100

    ADDED: channels SUPERCOLLIDER_MESSAGE and SUPERCOLLIDER_RESPONSE

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,7 +1,11 @@
 module.exports = {
   channels: {
     APP_INFO: "app_info",
+    // SuperCollider
     START_SUPERCOLLIDER: "start_supercollider",
+    SUPERCOLLIDER_MESSAGE: "supercollider_message",
+    SUPERCOLLIDER_RESPONSE: "supercollider_response",
+    // Websocket Server
     START_WS_SERVER: "start_ws_server",
     WEBSOCKET_OPEN: "websocket_open",
     WEBSOCKET_CLOSED: "websocket_closed",
