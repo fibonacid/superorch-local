a25b1636d9b5415b23cc5bd3a5990e13d0cd27a1
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-14 18:52:32 +0100

    ADDED: send error message when scQuery is undefined

--- src/react/saga/server/responses/getScQueryDataResponse.js
+++ src/react/saga/server/responses/getScQueryDataResponse.js
@@ -5,11 +5,22 @@ import {
 } from "../../../actions/server/responses/getScQueryDataResponse";
 import { s_transmit } from "../../../actions/server/transmit";
 import { selectScQuery } from "../../../reducers/root";
+import { statusCodes } from "../../../utils/constants";
 
 export function* s_getScQueryDataResponseSaga(clientId, scqId) {
   try {
     // Get list of users from state
     const scqData = yield select(state => selectScQuery(state, scqId));
+
+    console.log("s_getScQueryDataResponseSaga", { scqData });
+
+    if (typeof scqData === "undefined") {
+      console.error(`scQuery with id ${scqId} doesn't exist`);
+      return yield put(
+        s_transmit(clientId, s_getScQueryDataError(400, statusCodes[400]))
+      );
+    }
+
     // Send it to the requesting client
     yield put(s_transmit(clientId, s_getScQueryDataSuccess(scqData)));
   } catch (error) {
