1d75f59ba80ab6f658434dbc338ebeb061f37274
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-16 16:59:38 +0100

    CHANGED: padding

--- src/react/components/Header/Header.js
+++ src/react/components/Header/Header.js
@@ -5,8 +5,7 @@ import LoginToggle from "../LoginToggle";
 
 const StyledContainer = styled.div`
   font-size: 18px;
-  font-weight: bold;
-  padding: 8px;
+  padding: 14px;
   flex: 0 0 auto;
   display: flex;
   justify-content: center;
