38344cff534af358a4d8616fc006c36bc4ed20c1
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-16 12:42:35 +0100

    CHANGED: transmit message to only loggedIn clients

--- src/react/saga/server/broadcast.js
+++ src/react/saga/server/broadcast.js
@@ -1,6 +1,7 @@
 import { channels } from "../../../shared/constants";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, select, all } from "redux-saga/effects";
 import { actionTypes } from "../../actions/actionTypes";
+import { selectClients } from "../../reducers/root";
 
 export function* s_broadcastWatcher() {
   yield takeLatest(actionTypes.S_BROADCAST, s_broadcastSaga);
@@ -8,11 +9,21 @@ export function* s_broadcastWatcher() {
 
 export function* s_broadcastSaga({ clientId, message }) {
   const { ipcRenderer } = window;
+  // Send to electron main process
   if (ipcRenderer) {
-    // Send to electron main process
-    yield ipcRenderer.send(channels.WEBSOCKET_BROADCAST, {
-      clientId,
-      message
-    });
+    // Get all clients
+    const clients = yield select(state => selectClients(state));
+
+    // Transmit to all clients that are logged in and that don't match the sender id.
+    yield all(
+      clients
+        .filter(client => client.isLoggedIn && client.id !== clientId)
+        .map(client =>
+          ipcRenderer.send(channels.WEBSOCKET_TRANSMIT, {
+            clientId: client.id,
+            message
+          })
+        )
+    );
   }
 }
