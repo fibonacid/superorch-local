8fd095e98b6069b3a3f5196d57b4826b669777ae
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-16 14:08:12 +0100

    ADDED: userList component

--- src/react/components/SideBar/SideBar.js
+++ src/react/components/SideBar/SideBar.js
@@ -1,34 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components/macro";
+import UserList from "../UserList/UserList";
 
 const StyledContainer = styled.div`
   border-right: solid 1px black;
   width: 200px;
   max-width: 300px;
   overflow-y: auto;
+  position: relative;
 `;
 
-const StyledList = styled.ul`
-  height: 100%;
-`;
-
-const StyledListItem = styled.li`
-  padding: 5px;
-  border-bottom: solid 1px black;
+const StyledFallback = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: grey;
 `;
 
 function SideBar(props) {
-  const { users } = props;
   return (
     <StyledContainer data-test={"SideBarComponent"}>
-      <StyledList data-test={"list"}>
-        {users.map((user, i) => (
-          <StyledListItem data-test={"list-item"} key={i}>
-            {user.name} #{user.id}
-          </StyledListItem>
-        ))}
-      </StyledList>
+      {props.users.length === 0 ? (
+        <StyledFallback>No users available</StyledFallback>
+      ) : (
+        <UserList users={props.users} />
+      )}
     </StyledContainer>
   );
 }
---
+++ src/react/components/UserList/UserList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  height: 100%;
+`;
+
+const StyledListItem = styled.li`
+  padding: 5px;
+  border-bottom: solid 1px black;
+`;
+
+function UserList(props) {
+  return (
+    <StyledList data-test={"list"}>
+      {props.users.map((user, i) => (
+        <StyledListItem data-test={"list-item"} key={i}>
+          {user.name} #{user.id}
+        </StyledListItem>
+      ))}
+    </StyledList>
+  );
+}
+
+export default UserList;
