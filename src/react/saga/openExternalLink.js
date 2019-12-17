3ddd8e43512580d55852ca75532f9cd76d1f0dbb
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-17 17:09:06 +0100

    ADDED: watcher saga for action OPEN_EXTERNAL_LINK

---
+++ src/react/saga/openExternalLink.js
@@ -0,0 +1,14 @@
+import { actionTypes } from "../actions/actionTypes";
+import { takeLatest } from "redux-saga/effects";
+
+export function* openExternalLinkWatcher() {
+  yield takeLatest(actionTypes.OPEN_EXTERNAL_LINK, openExternalLinkSaga);
+}
+
+export function* openExternalLinkSaga(action) {
+  const { ipcRenderer } = window;
+  // If app runs on electron
+  if (ipcRenderer) {
+    ipcRenderer.send(channels.OPEN_EXT_LINK, action.url);
+  }
+}
--- src/react/saga/root.js
+++ src/react/saga/root.js
@@ -2,7 +2,13 @@ import { all } from "redux-saga/effects";
 import { clientSagas } from "./client";
 import { serverSagas } from "./server";
 import { broadcastSagas } from "./broadcast";
+import { openExternalLinkWatcher } from "./openExternalLink";
 
 export function* root() {
-  yield all([clientSagas(), serverSagas(), broadcastSagas()]);
+  yield all([
+    openExternalLinkWatcher(),
+    clientSagas(),
+    serverSagas(),
+    broadcastSagas()
+  ]);
 }
