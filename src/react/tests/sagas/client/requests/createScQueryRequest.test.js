3270937cc8f9da20fc75491ab586bf5c6956cc69
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-19 17:33:48 +0100

    ADDED: tests for success, error and timeout

--- src/react/tests/sagas/client/requests/createScQueryRequest.test.js
+++ src/react/tests/sagas/client/requests/createScQueryRequest.test.js
@@ -1,6 +1,7 @@
 import { expectSaga } from "redux-saga-test-plan";
 import { actionTypes } from "../../../../actions/actionTypes";
 import { c_createScQueryRequestWatcher } from "../../../../sagas/client/requests/createScQueryRequest";
+import { c_createScQueryRequestSaga } from "../../../../sagas/client/requests/createScQueryRequest";
 
 describe("c_createScQueryRequest", () => {
   it("should send a request", () => {
@@ -22,5 +23,80 @@ describe("c_createScQueryRequest", () => {
         // Start the test. Returns a Promise.
         .run()
     );
+  });
+
+  describe("when a result is received", () => {
+    it("should dispatch a success action", () => {
+      const action = {
+        type: actionTypes.C_CREATE_SC_QUERY_REQUEST,
+        scqData: {}
+      };
+      return (
+        expectSaga(c_createScQueryRequestSaga, action)
+          // Assert that the `put` will eventually happen.
+          .put.like({
+            action: {
+              type: actionTypes.C_CREATE_SC_QUERY_SUCCESS,
+              scqId: 1,
+              scqData: {}
+            }
+          })
+          // Dispatch any actions that the saga will `take`.
+          .dispatch({
+            type: actionTypes.S_CREATE_SC_QUERY_SUCCESS,
+            scqId: 1,
+            scqData: {}
+          })
+          // Start the test. Returns a Promise.
+          .run()
+      );
+    });
+  });
+
+  describe("when an error is received", () => {
+    it("should dispatch an error action", () => {
+      const action = {
+        type: actionTypes.C_CREATE_SC_QUERY_REQUEST,
+        scqData: {}
+      };
+      return (
+        expectSaga(c_createScQueryRequestSaga, action)
+          // Assert that the `put` will eventually happen.
+          .put.like({
+            action: {
+              type: actionTypes.C_CREATE_SC_QUERY_ERROR,
+              error: {}
+            }
+          })
+          // Dispatch any actions that the saga will `take`.
+          .dispatch({
+            type: actionTypes.S_CREATE_SC_QUERY_ERROR,
+            error: {}
+          })
+          // Start the test. Returns a Promise.
+          .run(500)
+      );
+    });
+  });
+
+  describe("when request takes too long to complete", () => {
+    it("should dispatch a timeout action", () => {
+      const action = {
+        type: actionTypes.C_CREATE_SC_QUERY_REQUEST,
+        scqData: {}
+      };
+      return (
+        expectSaga(c_createScQueryRequestSaga, action)
+          // Assert that the `put` will eventually happen.
+          .put.like({
+            action: {
+              type: actionTypes.C_CREATE_SC_QUERY_TIMEOUT
+            }
+          })
+          .delay(2500)
+          // Start the test. Returns a Promise.
+          .run(3000)
+      );
+    });
   });
 });
