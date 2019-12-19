8202a9db34d6a9476e562fc748cc00b93adbbe3f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-19 17:10:48 +0100

    ADDED: test for "when a result is received"

--- src/react/tests/sagas/client/requests/createDocumentRequest.test.js
+++ src/react/tests/sagas/client/requests/createDocumentRequest.test.js
@@ -1,6 +1,9 @@
-import { expectSaga } from "redux-saga-test-plan";
+import { expectSaga, testSaga } from "redux-saga-test-plan";
 import { actionTypes } from "../../../../actions/actionTypes";
-import { c_createDocumentRequestWatcher } from "../../../../sagas/client/requests/createDocumentRequest";
+import {
+  c_createDocumentRequestSaga,
+  c_createDocumentRequestWatcher
+} from "../../../../sagas/client/requests/createDocumentRequest";
 
 describe("c_createDocumentRequest", () => {
   it("should send a request", () => {
@@ -23,4 +26,34 @@ describe("c_createDocumentRequest", () => {
         .run()
     );
   });
+
+  describe("when a result is received", () => {
+    it("should dispatch a success action", () => {
+      const action = {
+        type: actionTypes.C_CREATE_DOCUMENT_REQUEST,
+        docData: {}
+      };
+      return (
+        expectSaga(c_createDocumentRequestSaga, action)
+          // Assert that the `put` will eventually happen.
+          .put.like({
+            action: {
+              type: actionTypes.C_CREATE_DOCUMENT_SUCCESS,
+              docId: 1
+            }
+          })
+          // Dispatch any actions that the saga will `take`.
+          .dispatch({
+            type: actionTypes.S_CREATE_DOCUMENT_SUCCESS,
+            docId: 1
+          })
+          // Start the test. Returns a Promise.
+          .run()
+      );
+    });
+  });
+
+  describe("when an error is received", () => {});
+
+  describe("when request takes too long to complete", () => {});
 });
