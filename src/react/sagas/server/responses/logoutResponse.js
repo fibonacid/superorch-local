27658bcde9bdc8a54de6477b19f82aef1a48e6cb
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-20 17:05:42 +0100

    ADDED: tests for s_logoutResponse saga

--- src/react/sagas/server/responses/logoutResponse.js
+++ src/react/sagas/server/responses/logoutResponse.js
@@ -1,4 +1,4 @@
-import { put, select } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import {
   s_logoutError,
   s_logoutSuccess
@@ -8,14 +8,21 @@ import { s_transmit } from "../../../actions/server/transmit";
 import { s_broadcast } from "../../../actions/server/broadcast";
 import { selectClient, selectUser } from "../../../reducers/root";
 import { s_updateClient } from "../../../actions/server/crudClients";
+import { testFunction } from "../../../utils/testing";
+import { statusCodes } from "../../../utils/constants";
 
 export function* s_logoutResponseSaga(clientId) {
   try {
+    // Call testFunction to enable test code injection
+    if (process.env.NODE_ENV === "test") {
+      yield call(testFunction);
+    }
     // Get client from the state
     const client = yield select(state => selectClient(state, clientId));
     // Get associated user
     const user = yield select(state => selectUser(state, client.userId));
 
+    // Mark client as logged in
     yield put(s_updateClient(clientId, { isLoggedIn: false }));
 
     // Transmit that the logout was successful
@@ -26,6 +33,7 @@ export function* s_logoutResponseSaga(clientId) {
   } catch (error) {
     // If there was an error:
     // Transmit error to the client that tried logging in.
-    yield put(s_transmit(clientId, s_logoutError(error.message)));
+    yield put(s_transmit(clientId, s_logoutError(500, statusCodes[500])));
+    process.env.NODE_ENV !== "test" && console.error(error);
   }
 }
---
+++ src/react/tests/sagas/server/responses/logoutResponse.test.js
@@ -0,0 +1,93 @@
+import { expectSaga } from "redux-saga-test-plan";
+import * as matchers from "redux-saga-test-plan/matchers";
+import { actionTypes } from "../../../../actions/actionTypes";
+import { s_logoutResponseSaga } from "../../../../sagas/server/responses/logoutResponse";
+import { testFunction } from "../../../../utils/testing";
+
+describe("s_logoutResponse saga", () => {
+  describe("when everything is alright", () => {
+    let saga;
+    const clientId = 0;
+    const userId = 1;
+    const userData = { foo: "bar" };
+    const newUser = {
+      ...userData,
+      id: userId
+    };
+
+    beforeEach(() => {
+      saga = expectSaga(s_logoutResponseSaga, clientId).withState({
+        server: {
+          clients: [{ id: 0, userId }]
+        },
+        client: {
+          users: [{ id: 1 }]
+        }
+      });
+    });
+
+    it("should mark the client as logged out", () => {
+      return saga
+        .put({
+          type: actionTypes.S_UPDATE_CLIENT,
+          clientId,
+          clientData: {
+            isLoggedIn: false
+          }
+        })
+        .run();
+    });
+
+    it("should broadcast that a user has left", () => {
+      return saga
+        .put({
+          type: actionTypes.S_BROADCAST,
+          clientId,
+          message: {
+            type: actionTypes.B_USER_LEFT,
+            userId
+          }
+        })
+        .run();
+    });
+
+    it("should transmit a success message", () => {
+      return saga
+        .put({
+          type: actionTypes.S_TRANSMIT,
+          clientId,
+          message: {
+            type: actionTypes.S_LOGOUT_SUCCESS,
+            userId
+          }
+        })
+        .run();
+    });
+  });
+
+  describe("when an error is raised", () => {
+    it.skip("should transmit an error 500 message to the client", () => {
+      const clientId = 0;
+      return expectSaga(s_logoutResponseSaga, clientId)
+        .provide([
+          [
+            matchers.call.fn(testFunction),
+            () => {
+              throw new Error("test");
+            }
+          ]
+        ])
+        .put.like({
+          action: {
+            type: actionTypes.S_TRANSMIT,
+            clientId,
+            message: {
+              type: actionTypes.S_LOGOUT_ERROR,
+              error: { status: 500 }
+            }
+          }
+        })
+        .run();
+    });
+  });
+});
