b3c65c0b71d97173c2749ff1b047dea758450e48
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-20 15:14:53 +0100

    ADDED: tests for s_getDocumentListResponse saga

--- src/react/sagas/server/responses/getDocumentListResponse.js
+++ src/react/sagas/server/responses/getDocumentListResponse.js
@@ -1,13 +1,18 @@
-import { put, select } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import {
   s_getDocumentListError,
   s_getDocumentListSuccess
 } from "../../../actions/server/responses/getDocumentListResponse";
 import { s_transmit } from "../../../actions/server/transmit";
 import { selectDocuments } from "../../../reducers/root";
+import { testFunction } from "../../../utils/testing";
+import { statusCodes } from "../../../utils/constants";
 
 export function* s_getDocumentListResponseSaga(clientId) {
   try {
+    if (process.env.NODE_ENV === "test") {
+      yield call(testFunction);
+    }
     // Get list of users from state
     const docList = yield select(state => selectDocuments(state));
     // Send it to the requesting client
@@ -15,7 +20,11 @@ export function* s_getDocumentListResponseSaga(clientId) {
   } catch (error) {
     // If there was an error:
     // Transmit error to the client that tried logging in.
-    yield put(s_transmit(clientId, s_getDocumentListError(error)));
-    console.error(error);
+    yield put(
+      s_transmit(clientId, s_getDocumentListError(500, statusCodes[500]))
+    );
+    if (process.env.NODE_ENV !== "test") {
+      console.error(error);
+    }
   }
 }
---
+++ src/react/tests/sagas/server/responses/getDocumentListResponse.test.js
@@ -0,0 +1,59 @@
+import { expectSaga } from "redux-saga-test-plan";
+import * as matchers from "redux-saga-test-plan/matchers";
+import { actionTypes } from "../../../../actions/actionTypes";
+import { testFunction } from "../../../../utils/testing";
+import { s_getDocumentListResponseSaga } from "../../../../sagas/server/responses/getDocumentListResponse";
+
+describe("s_getDocumentListResponse saga", () => {
+  describe("when everything is alright", () => {
+    let saga;
+    const clientId = 0;
+    const docList = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
+    beforeEach(() => {
+      saga = expectSaga(s_getDocumentListResponseSaga, clientId).withState({
+        client: {
+          documents: docList
+        }
+      });
+    });
+
+    it("should transmit the document list", () => {
+      return saga
+        .put({
+          type: actionTypes.S_TRANSMIT,
+          clientId,
+          message: {
+            type: actionTypes.S_GET_DOCUMENT_LIST_SUCCESS,
+            docList
+          }
+        })
+        .run();
+    });
+  });
+
+  describe("when an error is raised", () => {
+    it("should transmit an error 500 message to the client", () => {
+      const clientId = 0;
+      return expectSaga(s_getDocumentListResponseSaga, clientId)
+        .provide([
+          [
+            matchers.call.fn(testFunction),
+            () => {
+              throw new Error("test");
+            }
+          ]
+        ])
+        .put.like({
+          action: {
+            type: actionTypes.S_TRANSMIT,
+            clientId,
+            message: {
+              type: actionTypes.S_GET_DOCUMENT_LIST_ERROR,
+              error: { status: 500 }
+            }
+          }
+        })
+        .run();
+    });
+  });
+});
