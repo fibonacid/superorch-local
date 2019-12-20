48d2e0343927675f413b67a39dff5db58d376b5f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-20 18:37:23 +0100

    ADDED: tests for s_updateDocumentData saga

--- src/react/sagas/server/responses/updateDocumentDataResponse.js
+++ src/react/sagas/server/responses/updateDocumentDataResponse.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import { put, select } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import {
   selectClient,
   selectUser,
@@ -12,9 +12,18 @@ import {
 } from "../../../actions/server/responses/updateDocumentDataResponse";
 import { s_broadcast } from "../../../actions/server/broadcast";
 import { b_documentUpdate } from "../../../actions/broadcast/documentUpdate";
+import { testFunction } from "../../../utils/testing";
+import { statusCodes } from "../../../utils/constants";
 
 export function* s_updateDocumentDataResponseSaga(clientId, docId, docData) {
   try {
+    // Call testFunction to enable test code injection
+    if (process.env.NODE_ENV === "test") {
+      yield call(testFunction);
+    }
+
+    console.log(yield select(state => state));
+
     // Get user associated with the client
     const client = yield select(state => selectClient(state, clientId));
     const user = yield select(state => selectUser(state, client.userId));
@@ -22,20 +31,20 @@ export function* s_updateDocumentDataResponseSaga(clientId, docId, docData) {
 
     // Check if document exists:
     if (!document) {
+      const message = `Document with is ${docId} doesn't exist`;
+      console.error(message);
       return yield put(
-        s_transmit(s_updateDocumentDataError(400, `Document doesn't exist`))
+        s_transmit(clientId, s_updateDocumentDataError(404, message))
       );
     }
 
     // Check if user owns the document:
     if (user.id !== document.userId) {
+      const message =
+        "You don't have the right permissions to update this document";
+      process.env.NODE_ENV !== "test" && console.error(message);
       return yield put(
-        s_transmit(
-          s_updateDocumentDataError(
-            400,
-            `You don't have the right permissions to update this document`
-          )
-        )
+        s_transmit(clientId, s_updateDocumentDataError(400, message))
       );
     }
 
@@ -49,7 +58,9 @@ export function* s_updateDocumentDataResponseSaga(clientId, docId, docData) {
     yield put(s_transmit(clientId, s_updateDocumentDataSuccess()));
   } catch (error) {
     // Transmit error message
-    yield put(s_transmit(clientId, s_updateDocumentDataError(500)));
+    yield put(
+      s_transmit(clientId, s_updateDocumentDataError(500, statusCodes[500]))
+    );
     console.error(error);
   }
 }
---
+++ src/react/tests/sagas/server/responses/updateDocumentDataResponse.test.js
@@ -0,0 +1,139 @@
+import _ from "lodash";
+import { expectSaga } from "redux-saga-test-plan";
+import * as matchers from "redux-saga-test-plan/matchers";
+import { actionTypes } from "../../../../actions/actionTypes";
+import { s_updateDocumentDataResponseSaga } from "../../../../sagas/server/responses/updateDocumentDataResponse";
+import { testFunction } from "../../../../utils/testing";
+
+describe("s_updateDocumentData saga", () => {
+  describe("when everything is alright", () => {
+    const clientId = 0;
+    const userId = 1;
+    const docId = 2;
+    const docData = { foo: "bar", id: 99, userId: 100 };
+
+    let saga;
+    beforeEach(() => {
+      saga = expectSaga(
+        s_updateDocumentDataResponseSaga,
+        clientId,
+        docId,
+        docData
+      ).withState({
+        server: {
+          clients: [{ id: clientId, userId }]
+        },
+        client: {
+          users: [{ id: userId }],
+          documents: [{ id: docId, userId }]
+        }
+      });
+    });
+
+    it("should broadcast a document update", () => {
+      return saga.put
+        .like({
+          action: {
+            type: actionTypes.S_BROADCAST,
+            clientId,
+            message: {
+              type: actionTypes.B_DOCUMENT_UPDATE
+            }
+          }
+        })
+        .run();
+    });
+    it("should transmit a success message", () => {
+      return saga.put
+        .like({
+          action: {
+            type: actionTypes.S_TRANSMIT,
+            clientId,
+            message: {
+              type: actionTypes.S_UPDATE_DOCUMENT_DATA_SUCCESS
+            }
+          }
+        })
+        .run();
+    });
+    it("should filter out id and userId from the received data", () => {
+      return saga
+        .put({
+          type: actionTypes.S_BROADCAST,
+          clientId,
+          message: {
+            type: actionTypes.B_DOCUMENT_UPDATE,
+            docId,
+            docData: _.omit(docData, ["id", "userId"])
+          }
+        })
+        .run();
+    });
+  });
+
+  describe("when document doesn't exist", () => {
+    const clientId = 0;
+    const userId = 1;
+    const docId = 99;
+    const docData = { foo: "bar" };
+
+    let saga;
+    beforeEach(() => {
+      saga = expectSaga(
+        s_updateDocumentDataResponseSaga,
+        clientId,
+        docId,
+        docData
+      ).withState({
+        server: {
+          clients: [{ id: clientId, userId }]
+        },
+        client: {
+          users: [{ id: userId }],
+          documents: [{ id: 2, userId }]
+        }
+      });
+    });
+
+    it("should transmit an error 404 to the client", () => {
+      return saga.put
+        .like({
+          action: {
+            type: actionTypes.S_TRANSMIT,
+            clientId,
+            message: {
+              type: actionTypes.S_UPDATE_DOCUMENT_DATA_ERROR,
+              error: { status: 404 }
+            }
+          }
+        })
+        .run();
+    });
+  });
+
+  describe("when an error is raised", () => {
+    it("should transmit an error 500 message to the client", () => {
+      const clientId = 0;
+      return expectSaga(s_updateDocumentDataResponseSaga, clientId)
+        .provide([
+          [
+            matchers.call.fn(testFunction),
+            () => {
+              throw new Error("test");
+            }
+          ]
+        ])
+        .put.like({
+          action: {
+            type: actionTypes.S_TRANSMIT,
+            clientId,
+            message: {
+              type: actionTypes.S_UPDATE_DOCUMENT_DATA_ERROR,
+              error: { status: 500 }
+            }
+          }
+        })
+        .run();
+    });
+  });
+});
