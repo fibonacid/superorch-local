e04fe0808b67fd92958915e7e7841456c8a14052
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-20 14:56:52 +0100

    ADDED: tests for s_createDocumentResponse saga

--- src/react/sagas/server/responses/createDocumentResponse.js
+++ src/react/sagas/server/responses/createDocumentResponse.js
@@ -4,32 +4,60 @@ import {
 } from "../../../actions/server/responses/createDocumentResponse";
 import { s_broadcast } from "../../../actions/server/broadcast";
 import { s_transmit } from "../../../actions/server/transmit";
-import { put, select } from "redux-saga/effects";
+import { put, select, call } from "redux-saga/effects";
 import { selectClient, selectUser } from "../../../reducers/root";
 import { b_documentCreated } from "../../../actions/broadcast/documentCreated";
+import { testFunction } from "../../../utils/testing";
+import { statusCodes } from "../../../utils/constants";
 
 let docCount = 10 * Math.floor(Math.random() * 10);
 
+/**
+ *
+ * @returns {number}
+ */
+export function generateDocId() {
+  docCount++;
+  return docCount;
+}
+
+/**
+ *
+ * @param clientId
+ * @param docData
+ * @returns {IterableIterator<PutEffect<{clientId, type, message}>|CallEffect|SelectEffect>}
+ */
 export function* s_createDocumentResponseSaga(clientId, docData) {
   docCount++;
   try {
+    if (process.env.NODE_ENV === "test") {
+      yield call(testFunction);
+    }
+
     // Get user associated with the client
     const client = yield select(state => selectClient(state, clientId));
     const user = yield select(state => selectUser(state, client.userId));
 
+    const docId = yield call(generateDocId);
+
     const newDoc = {
       ...docData,
-      id: docCount,
+      id: docId,
       userId: user.id
     };
 
     // Broadcast new document
-    yield put(s_broadcast(clientId, b_documentCreated(newDoc.id, newDoc)));
+    yield put(s_broadcast(clientId, b_documentCreated(docId, newDoc)));
 
     // Transmit success message
-    yield put(s_transmit(clientId, s_createDocumentSuccess(newDoc.id, newDoc)));
+    yield put(s_transmit(clientId, s_createDocumentSuccess(docId, newDoc)));
   } catch (error) {
     // Send error message
-    yield put(s_transmit(clientId, s_createDocumentError(error)));
+    yield put(
+      s_transmit(clientId, s_createDocumentError(500, statusCodes[500]))
+    );
+    if (process.env.NODE_ENV !== "test") {
+      console.error(error);
+    }
   }
 }
---
+++ src/react/tests/sagas/server/responses/createDocumentResponse.test.js
@@ -0,0 +1,91 @@
+import { expectSaga } from "redux-saga-test-plan";
+import * as matchers from "redux-saga-test-plan/matchers";
+import { actionTypes } from "../../../../actions/actionTypes";
+import {
+  s_createDocumentResponseSaga,
+  generateDocId
+} from "../../../../sagas/server/responses/createDocumentResponse";
+import { testFunction } from "../../../../utils/testing";
+
+describe("s_createDocumentResponse saga", () => {
+  describe("when everything is alright", () => {
+    let saga;
+    const clientId = 0;
+    const docId = 1;
+    const docData = { foo: "bar" };
+    const userId = 1;
+    const newDoc = {
+      ...docData,
+      id: docId,
+      userId
+    };
+
+    beforeEach(() => {
+      saga = expectSaga(s_createDocumentResponseSaga, clientId, docData)
+        .withState({
+          server: {
+            clients: [{ id: 0, userId }]
+          },
+          client: {
+            users: [{ id: 1 }]
+          }
+        })
+        .provide([[matchers.call.fn(generateDocId), docId]]);
+    });
+
+    it("should broadcast the new document", () => {
+      return saga
+        .put({
+          type: actionTypes.S_BROADCAST,
+          clientId,
+          message: {
+            type: actionTypes.B_DOCUMENT_CREATED,
+            docId,
+            docData: newDoc
+          }
+        })
+        .run();
+    });
+
+    it("should transmit a success message", () => {
+      return saga
+        .put({
+          type: actionTypes.S_TRANSMIT,
+          clientId,
+          message: {
+            type: actionTypes.S_CREATE_DOCUMENT_SUCCESS,
+            docId,
+            docData: newDoc
+          }
+        })
+        .run();
+    });
+  });
+
+  describe("when an error is raised", () => {
+    it("should transmit an error 500 message to the client", () => {
+      const clientId = 0;
+      const docData = {};
+      return expectSaga(s_createDocumentResponseSaga, clientId, docData)
+        .provide([
+          [
+            matchers.call.fn(testFunction),
+            () => {
+              throw new Error("test");
+            }
+          ]
+        ])
+        .put.like({
+          action: {
+            type: actionTypes.S_TRANSMIT,
+            clientId,
+            message: {
+              type: actionTypes.S_CREATE_DOCUMENT_ERROR,
+              error: { status: 500 }
+            }
+          }
+        })
+        .run();
+    });
+  });
+});
