7275e65a81f16cd6504e5d2f20011873474bccf9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-24 23:38:00 +0100

    REMOVED: url bar and login toggle. ADDED: link to join page.

--- src/react/components/Header/Header.js
+++ src/react/components/Header/Header.js
@@ -1,22 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components/macro";
 import URLBar from "../URLBar/index";
 import LoginToggle from "../LoginToggle";
 
 const StyledContainer = styled.div`
-  font-size: 18px;
   padding: 14px;
   flex: 0 0 auto;
   display: flex;
-  justify-content: center;
+  flex-direction: row-reverse;
   align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 16px;
+  text-decoration: none;
+  color: black;
 `;
 
 function Header(props) {
   return (
     <StyledContainer data-test={"HeaderComponent"}>
-      <URLBar />
-      <LoginToggle />
+      <StyledLink to="/join">Join</StyledLink>
     </StyledContainer>
   );
 }
