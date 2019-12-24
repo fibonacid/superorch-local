32857ef2df25184550f69e835714a1264dd98201
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-24 16:51:30 +0100

    Revert "ADDED: actions S_LOGIN_AUTH_REQUIRED and similar"
    This reverts commit b3300bec9ece506772770986c63a89906888d289.

--- src/react/actions/server/responses/loginResponse.js
+++ src/react/actions/server/responses/loginResponse.js
@@ -9,16 +9,3 @@ export const s_loginError = (status, message) => ({
   type: actionTypes.S_LOGIN_ERROR,
   error: { status, message }
 });
-
-export const s_loginAuthRequired = () => ({
-  type: actionTypes.S_LOGIN_AUTH_REQUIRED
-});
-
-export const s_loginAuthSuccess = () => ({
-  type: actionTypes.S_LOGIN_AUTH_SUCCESS
-});
-
-export const s_loginAuthError = (status, message) => ({
-  type: actionTypes.S_LOGIN_AUTH_ERROR,
-  error: { status, message }
-});
