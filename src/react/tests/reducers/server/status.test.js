431898c4d871d97f3ae9c82a2431307548dcde85
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-24 18:13:21 +0100

    ADDED: tests for handling of action S_AUTH_WITH_PASSWORD and S_CHANGE_PASSWORD

--- src/react/tests/reducers/server/status.test.js
+++ src/react/tests/reducers/server/status.test.js
@@ -21,4 +21,28 @@ describe("status reducer", () => {
       wsEndpoint: "ws://127.0.0.1:8000"
     });
   });
+
+  it("should handle S_AUTH_WITH_PASSWORD", () => {
+    const flag = true;
+    expect(
+      reducer(undefined, {
+        type: actionTypes.S_AUTH_WITH_PASSWORD,
+        flag
+      })
+    ).toMatchObject({
+      requirePassword: flag
+    });
+  });
+
+  it("should handle S_CHANGE_PASSWORD", () => {
+    const password = "test";
+    expect(
+      reducer(undefined, {
+        type: actionTypes.S_CHANGE_PASSWORD,
+        password
+      })
+    ).toMatchObject({
+      password
+    });
+  });
 });
