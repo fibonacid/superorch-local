fd0353b478200270790a1a71b38011bbe3a719bd
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-24 19:01:31 +0100

    ADDED: function setPassword

--- src/react/components/StatusBar/index.js
+++ src/react/components/StatusBar/index.js
@@ -1,9 +1,15 @@
 import { connect } from "react-redux";
 import StatusBar from "./StatusBar";
 import { openExternalLink } from "../../actions/openExternalLink";
+import { s_authWithPassword } from "../../actions/server/authWith";
+import { s_changePassword } from "../../actions/server/changePassword";
 
 const mapDispatchToProps = dispatch => ({
-  openExternalLink: url => dispatch(openExternalLink(url))
+  openExternalLink: url => dispatch(openExternalLink(url)),
+  setPassword: function(password) {
+    dispatch(s_changePassword(password));
+    dispatch(s_authWithPassword());
+  }
 });
 
 const mapStateToProps = state => ({
