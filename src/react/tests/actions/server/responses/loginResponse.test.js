b3300bec9ece506772770986c63a89906888d289
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-24 15:40:26 +0100

    ADDED: actions S_LOGIN_AUTH_REQUIRED and similar

--- src/react/tests/actions/server/responses/loginResponse.test.js
+++ src/react/tests/actions/server/responses/loginResponse.test.js
@@ -1,7 +1,10 @@
 import { actionTypes } from "../../../../actions/actionTypes";
 import {
   s_loginSuccess,
-  s_loginError
+  s_loginError,
+  s_loginAuthRequired,
+  s_loginAuthError,
+  s_loginAuthSuccess
 } from "../../../../actions/server/responses/loginResponse";
 import { statusCodes } from "../../../../utils/constants";
 
@@ -21,6 +24,33 @@ describe("s_loginError action", () => {
     const message = statusCodes[500];
     expect(s_loginError(status, message)).toEqual({
       type: actionTypes.S_LOGIN_ERROR,
+      error: { status, message }
+    });
+  });
+});
+
+describe("s_loginAuthRequired action", () => {
+  it("should create an action to communicate that authentication is required", () => {
+    expect(s_loginAuthRequired()).toEqual({
+      type: actionTypes.S_LOGIN_AUTH_REQUIRED
+    });
+  });
+});
+
+describe("s_loginAuthRequired action", () => {
+  it("should create an action to communicate that authentication succeeded", () => {
+    expect(s_loginAuthSuccess()).toEqual({
+      type: actionTypes.S_LOGIN_AUTH_SUCCESS
+    });
+  });
+});
+
+describe("s_loginAuthError action", () => {
+  it("should create an action to communicate that authentication failed", () => {
+    const status = 500;
+    const message = statusCodes[500];
+    expect(s_loginAuthError(status, message)).toEqual({
+      type: actionTypes.S_LOGIN_AUTH_ERROR,
       error: { status, message }
     });
   });
