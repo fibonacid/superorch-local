0ee80665f064ef189c02dc25c768832e41e94a93
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-26 22:09:48 +0100

    ADDED: export selectors

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -1,9 +1,15 @@
-import documents from "./documents";
-import scQueries from "./scQueries";
-import users from "./users";
+import users, * as fromUsers from "./users";
+import documents, * as fromDocuments from "./documents";
+import scQueries, * as fromScQueries from "./scQueries";
 
 export default {
   documents,
   scQueries,
   users
+};
+
+export const selectors = {
+  selectUser: fromUsers.selectUser,
+  selectDocument: fromDocuments.selectDocument,
+  selectScQuery: fromScQueries.selectScQuery
 };
