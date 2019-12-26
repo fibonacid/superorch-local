706c0c7f6c3c8887ca2554b36603b28071cf28c6
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-26 19:21:43 +0100

    ADDED: reducers from shared/reducers directory

--- src/server/reducers/root.js
+++ src/server/reducers/root.js
@@ -1,5 +1,12 @@
 import { combineReducers } from "redux";
+import sharedReducers from "../../shared/reducers";
 
-const root = combineReducers({});
+const { users, documents, scQueries } = sharedReducers;
+
+const root = combineReducers({
+  users,
+  documents,
+  scQueries
+});
 
 export default root;
