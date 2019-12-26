f77e59903b54f30020180e8faf7a85e1992c3c56
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-26 22:34:51 +0100

    ADDED: express + ws server

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,30 @@
 import configureStore from "./store";
+import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
+require("dotenv").config();
+
+//
+//  Create redux store.
+//
 const store = configureStore({});
 
-console.log(store.getState());
+//
+//  Create express app.
+//
+const app = express();
+
+//
+// Create HTTP server.
+//
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+
+//
+// Start the server.
+//
+const port = process.env.SERVER_PORT || 8000;
+server.listen(port, function() {
+  console.log("Listening on http://localhost:%s", port);
+});
