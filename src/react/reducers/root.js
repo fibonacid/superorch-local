c357144fff23d4ec074241d4100b88290ade2e53
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-26 22:10:35 +0100

    ADDED: export selectors

--- src/react/reducers/root.js
+++ src/react/reducers/root.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import sharedReducers from "../../shared/reducers";
+import reducers, { selectors } from "../../shared/reducers";
 import base from "./base";
 import flash from "./flash";
 
@@ -10,3 +10,26 @@ const root = combineReducers({
 });
 
 export default root;
+
+export const selectBase = (state) => state.base;
+
+//
+//  selectors from shared directory
+//
+
+export const selectUsers = state => state.users;
+export const selectDocuments = state => state.documents;
+export const selectScQueries = state => state.scQueries;
+
+export const selectDocument = (state, id) => {
+  selectors.selectDocument(selectDocuments(state), id);
+};
+
+export const selectUser = (state, id) => {
+  selectors.selectUser(selectUsers(state), id);
+};
+
+export const selectScQuery = (state, id) => {
+  selectors.selectScQuery(selectScQueries(state), id);
+}
+
